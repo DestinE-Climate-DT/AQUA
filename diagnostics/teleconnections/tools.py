6711b8f4e8fa366765019ac0d2ba1c33e6e3080a
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-01-16 17:51:06 +0100

    draft package structure

---
+++ diagnostics/teleconnections/tools.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+def season_mean(ds, calendar='standard'):
+    # DataArray with number of days in each month
+    month_length = ds.time.dt.days_in_month
+
+    # Calculate the weights by grouping by 'time.season'
+    weights = month_length.groupby("time.season") / month_length.groupby("time.season").sum()
+
+    # Test that the sum of the weights for each season is 1.0
+    np.testing.assert_allclose(weights.groupby("time.season").sum().values, np.ones(4))
+
+    # Calculate the weighted average
+    return (ds * weights).groupby("time.season").sum(dim="time")
