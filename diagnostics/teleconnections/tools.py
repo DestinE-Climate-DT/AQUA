e2b4434620f5d6e1aab00f300e5232e6bb26668a
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-01-19 16:38:34 +0100

    NAO index evaluation test

--- diagnostics/teleconnections/tools.py
+++ diagnostics/teleconnections/tools.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 
+'''
 def season_mean(ds, calendar='standard'):
     # DataArray with number of days in each month
     month_length = ds.time.dt.days_in_month
@@ -14,3 +15,22 @@ def season_mean(ds, calendar='standard'):
 
     # Calculate the weighted average
     return (ds * weights).groupby("time.season").sum(dim="time")
+'''
+
+def lon_check (lon):
+    """ 
+    Convert longitude to [0,360] range.
+
+    Parameters
+    ----------
+    lon : float
+
+    Returns
+    -------
+    float
+    """
+    if (lon<0):
+        print('ok')
+        lon = (lon+180) % 360 
+        print(lon)
+    return lon
