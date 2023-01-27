6ec5ca2f11cfcda6402f79a29ac6c12f88126769
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-01-27 15:43:47 +0100

    regional mean index function + rolling window update

--- diagnostics/teleconnections/index.py
+++ diagnostics/teleconnections/index.py
@@ -1,3 +1,7 @@
+'''
+This module contains functions to evaluate teleconnection indices 
+for different teleconnections.
+'''
 import xarray
 from tools import *
 
@@ -7,14 +11,12 @@ def station_based_index(field, namelist, telecname, months_window = 3):
 
     Args:
         field (DataArray):        field over which evaluate the index
-        namelist:
+        namelist:                 teleconnection yaml infos
         telecname (str):          name of the teleconnection to be evaluated
         months_window (int, opt): months for rolling average, default is 3
 
     Returns:
         indx (DataArray): standardized station based index
-
-    Raises:
     """
     # 1. -- Monthly field average and anomalies--
     field_av = field.groupby("time.month").mean(dim="time")
@@ -23,7 +25,6 @@ def station_based_index(field, namelist, telecname, months_window = 3):
     # 2. -- Acquiring latitude and longitude of stations --
     lon1 = lon_180_to_360(namelist[telecname]['lon1'])
     lat1 = namelist[telecname]['lat1']
-
     lon2 = lon_180_to_360(namelist[telecname]['lon2'])
     lat2 = namelist[telecname]['lat2']
 
@@ -40,7 +41,36 @@ def station_based_index(field, namelist, telecname, months_window = 3):
     mean_ma = diff_ma.mean()
     std_ma  = diff_ma.std()
 
-    # 6. -- Evaluate the index --
+    # 6. -- Evaluate the index and rename the variable in the DataArray --
     indx = (diff_ma-mean_ma)/std_ma
+    indx = indx.rename('index')
+    
+    return indx
+
+def regional_mean_index(field, namelist, telecname, months_window = 3):
+    """
+    Evaluate index based on a for a teleconnection.
+
+    Args:
+        field (DataArray):        field over which evaluate the index
+        namelist:                 teleconnection yaml infos
+        telecname (str):          name of the teleconnection to be evaluated
+        months_window (int, opt): months for rolling average, default is 3
+
+    Returns:
+        indx (DataArray): standardized index
+    """
+    # 1. -- Acquire coordinates --
+    lonW = lon_180_to_360(namelist[telecname]['lonW']) # conversion can be inserted as a check in other funcs using coordinates
+    lonE = lon_180_to_360(namelist[telecname]['lonE'])
+    latN = namelist[telecname]['latN']
+    latS = namelist[telecname]['latS']
+
+    # 2. -- Evaluate mean value of the field and then the rolling mean --
+    field_mean = wgt_area_mean(field, latN, latS, lonW, lonE)
+    field_mean = field_mean.rolling(time=months_window).mean()
+
+    # 3. -- Additional operations (WIP) --
+    indx = field_mean
 
     return indx
