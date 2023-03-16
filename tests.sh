0a03bcc62253b5dd243f47e8563835322220979e
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-03-16 07:08:42 +0100

    separate jobs for aqua and teleconnections ci

--- tests.sh
+++ tests.sh
@@ -16,6 +16,3 @@ cp ./config/config.yaml ./config/config.yaml.bak
 sed -i "/^machine:/c\\machine: ci" "./config/config.yaml"
 python -m pytest ./tests/test_basic.py
 mv ./config/config.yaml.bak ./config/config.yaml
-
-# teleconnections tests
-python -m pytest ./tests/teleconnections/test_teleconnections.py
