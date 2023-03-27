7eb4c2c2a47c905e52825f90678ed902c9c1fb93
Natalia Nazarova <nnazarova@polito.it>
2023-03-27 04:56:06 +0200

    bash file modifiied

--- aqua_setup_and_run.sh
+++ aqua_setup_and_run.sh
@@ -4,7 +4,7 @@
 conda activate base
 
 # Run your Python script
-python conda_req_for_yml_UPD.py/conda_req_for_yml.py
+python requirements/conda_req_for_yml.py
 
 # Install packages required for the environment
 conda env create -v  --file environment.yml
