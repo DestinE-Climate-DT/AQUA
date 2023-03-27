80c3192101f5b9f2dcee9108903c6928d85c4632
Natalia Nazarova <nnazarova@polito.it>
2023-03-27 03:51:48 +0200

    Setup.py file updated. Now shared pip and conda dependencies import to Setup.py  by function load_requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import itertools
+
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+def load_requirements(fname):
+    install_reqs = parse_requirements(fname, session="test")
+    
+    try:
+        requirements = [str(ir.req) for ir in install_reqs]
+    except:
+        requirements = [str(ir.requirement) for ir in install_reqs]
+
+    return requirements
+
+dependencies = {
+  'extra_requirements'  : ['cfgrib', 'jinja2', 'pandas<2', 'pyYAML', 'urllib3<1.27'],
+  'remote_requirements' : ['smmregrid @ git+https://github.com/jhardenberg/smmregrid']
+}
 
 setup(name='aqua',
-      version='0.0.1',
+      version='0.0.5',
       description='AQUA; a model evaluation framework for high-resolution climate model simulations',
       author='The AQUA team',
       author_email='p.davini@isac.cnr.it',
@@ -11,21 +34,10 @@ setup(name='aqua',
       python_requires='>=3.9, <3.11',
       packages=['aqua'],
       install_requires=[
-        'cfgrib',
-        'dask',
-        'docker',
-        'gribscan',
-        'ecCodes',
-        'intake',
-        'intake-esm<=2021.8.17',
-        'intake-xarray',
-        'jinja2',
-        'metpy',
-        'numpy',
-        'pandas',
-        'pyYAML',
-        'sparse', 
-        'xarray',
-        'smmregrid @ git+https://github.com/jhardenberg/smmregrid'
-      ]
-    )
+         #dependencies['extra_requirements'],
+         load_requirements("requirements/conda_requirements.txt"),
+         load_requirements("requirements/pip_requirements.txt"),
+         dependencies['extra_requirements'],
+         #dependencies['remote_requirements'],
+         ]
+      )
