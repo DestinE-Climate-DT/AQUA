618a951381cef5991eb3a904793618c142e93a84
Natalia Nazarova <b382267@levante6.lvt.dkrz.de>
2023-03-30 05:32:55 +0200

    enviroment files updated

--- diagnostics/tropical_rainfall/env_setup.sh
+++ diagnostics/tropical_rainfall/env_setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+current_dir=$(pwd)
 
-#conda install  conda-merge 
+conda install  conda-merge 
 
-conda-merge ../../environment.yml  enviroment/env-tropical-rainfall.yml >> enviroment/merged.yml
+conda-merge ../../environment.yml  enviroment/env-tropical-rainfall.yml > enviroment/merged.yml
 
 conda env create -f enviroment/merged.yml
+
+conda activate tropical-rainfall
