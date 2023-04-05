c2511281890d051ddd10b770d8a760b65789f115
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-04-05 12:57:33 +0200

    yml to yaml, linting and **kwargs for plot funcs

--- diagnostics/teleconnections/plots.py
+++ diagnostics/teleconnections/plots.py
@@ -5,8 +5,9 @@ import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import xarray as xr
 
-def set_layout(fig, ax,title=None,xlabel=None,ylabel=None,xlog=False,ylog=False,
-               xlim=None,ylim=None):
+
+def set_layout(fig, ax, title=None, xlabel=None, ylabel=None, xlog=False,
+               ylog=False, xlim=None, ylim=None):
     """
     Set the layout of the plot
 
@@ -16,8 +17,10 @@ def set_layout(fig, ax,title=None,xlabel=None,ylabel=None,xlog=False,ylog=False,
         title (str,opt):        title of the plot
         xlabel (str,opt):       label of the x axis
         ylabel (str,opt):       label of the y axis
-        xlog (bool,opt):        enable or disable x axis log scale, default is False
-        ylog (bool,opt):        enable or disable y axis log scale, default is False
+        xlog (bool,opt):        enable or disable x axis log scale,
+                                default is False
+        ylog (bool,opt):        enable or disable y axis log scale,
+                                default is False
         xlim (tuple,opt):       x axis limits
         ylim (tuple,opt):       y axis limits
 
@@ -39,15 +42,16 @@ def set_layout(fig, ax,title=None,xlabel=None,ylabel=None,xlog=False,ylog=False,
         ax.set_xlim(xlim)
     if ylim:
         ax.set_ylim(ylim)
-    
+
     return fig, ax
 
-def cor_plot(indx,field,plot=True,projection_type='PlateCarree',
-             title=None,xlabel=None,ylabel=None,xlog=False,ylog=False,
-             contour=False,levels=8,save=False,outputdir='./',filename='cor.png'):
+
+def cor_plot(indx, field, plot=True, projection_type='PlateCarree',
+             contour=False, levels=8, save=False, outputdir='./',
+             filename='cor.png', **kwargs):
     """
-    Evaluate and plot correlation map of a teleconnection index 
-    and a DataArray field
+    Evaluate and plot correlation map of a teleconnection index
+    and a DataArray field.
 
     Args:
         indx (DataArray):       index DataArray
@@ -55,15 +59,19 @@ def cor_plot(indx,field,plot=True,projection_type='PlateCarree',
         projection_type (str):  projection style for cartopy
                                 If a wrong one is provided, it will fall back
                                 to PlateCarree
-        plot (bool):            enable or disable the plot output, true by default
-        title (str,opt):        title of the plot
-        xlabel (str,opt):       label of the x axis
-        ylabel (str,opt):       label of the y axis
-        xlog (bool,opt):        enable or disable x axis log scale, default is False
-        ylog (bool,opt):        enable or disable y axis log scale, default is False
-        contour (bool,opt):     enable or disable contour plot, default is False
+        plot (bool):            enable or disable the plot output,
+                                true by default
+        contour (bool,opt):     enable or disable contour plot,
+                                default is False
         levels (int,opt):       number of contour levels, default is 8
-    
+        save (bool,opt):        enable or disable saving the figure,
+                                default is False
+        outputdir (str,opt):    output directory for the figure,
+                                default is './'
+        filename (str,opt):     name of the figure file,
+                                default is 'cor.png'
+        **kwargs:               additional arguments for set_layout
+
     Returns:
         reg (DataArray):        DataArray for regression map
         fig (Figure,opt):       Figure object
@@ -77,22 +85,23 @@ def cor_plot(indx,field,plot=True,projection_type='PlateCarree',
     }
 
     # 2. -- Evaluate the map --
-    cor = xr.corr(indx,field, dim="time")
-    
+    cor = xr.corr(indx, field, dim="time")
+
     # 3. -- Plot the regression map --
     proj = projection_types.get(projection_type, ccrs.PlateCarree())
 
     if plot:
-        fig, ax = plt.subplots(subplot_kw={'projection': proj},figsize=(8,4))
-        
+        fig, ax = plt.subplots(subplot_kw={'projection': proj}, figsize=(8, 4))
+
         ax.coastlines()
         if contour:
-            cor.plot.contourf(ax=ax, transform=ccrs.PlateCarree(),levels=levels)
+            cor.plot.contourf(ax=ax, transform=ccrs.PlateCarree(),
+                              levels=levels)
         else:
             cor.plot(ax=ax, transform=ccrs.PlateCarree())
-        
-        set_layout(fig, ax, title=title, xlabel=xlabel, ylabel=ylabel, xlog=xlog, ylog=ylog)
-        
+
+        set_layout(fig, ax, **kwargs)
+
         # 4. -- Save the figure --
         if save:
             fig.savefig(outputdir + filename)
@@ -101,51 +110,57 @@ def cor_plot(indx,field,plot=True,projection_type='PlateCarree',
     else:
         return cor
 
-def hovmoller_plot(infile,dim='lon',title=None, xlabel=None,ylabel=None,
-                   contour=True,levels=8,xlog=False,ylog=False,xlim=None,
-                   ylim=None,invert_axis=False,save=False, outputdir='./',
-                   filename='hovmoller.png'):
+
+def hovmoller_plot(infile, dim='lon', contour=True, levels=8,
+                   xlabel=None, ylabel=None, title=None,
+                   invert_axis=False, save=False, outputdir='./',
+                   filename='hovmoller.png', **kwargs):
     '''
     Args:
         infile (DataArray):     DataArray to be plot
         dim (str,opt):          dimension to be averaged over, default is 'lon'
-        title (str,opt):        title of the plot
-        xlabel (str,opt):       label of the x axis
-        ylabel (str,opt):       label of the y axis
         contour (bool,opt):     enable or disable contour plot, default is True
         levels (int,opt):       number of contour levels, default is 8
-        xlog (bool,opt):        enable or disable x axis log scale, default is False
-        ylog (bool,opt):        enable or disable y axis log scale, default is False
-        xlim (tuple,opt):       x axis limits
-        ylim (tuple,opt):       y axis limits
-        invert_axis (bool,opt): enable or disable axis inversion, default is False
-        save (bool,opt):        enable or disable saving the figure, default is False
-        outputdir (str,opt):    output directory for the figure, default is './'
-        filename (str,opt):     filename for the figure, default is 'hovmoller.png'
-    
+        xlabel (str,opt):       label of the x axis
+        ylabel (str,opt):       label of the y axis
+        title (str,opt):        title of the plot
+        invert_axis (bool,opt): enable or disable axis inversion,
+                                default is False
+        save (bool,opt):        enable or disable saving the figure,
+                                default is False
+        outputdir (str,opt):    output directory for the figure,
+                                default is './'
+        filename (str,opt):     filename for the figure,
+                                default is 'hovmoller.png'
+        **kwargs:               additional arguments for set_layout
+
     Returns:
         fig (Figure):           Figure object
         ax (Axes):              Axes object
     '''
-    infile_mean = infile.mean(dim=dim,keep_attrs=True)
+    infile_mean = infile.mean(dim=dim, keep_attrs=True)
 
     fig, ax = plt.subplots(figsize=(12, 8))
 
     # Contour or pcolormesh plot
     if contour:
         if invert_axis:
-            im = ax.contourf(infile_mean.coords[infile_mean.dims[-1]], infile_mean.coords['time'], 
-                             infile_mean,levels=levels)
+            im = ax.contourf(infile_mean.coords[infile_mean.dims[-1]],
+                             infile_mean.coords['time'], infile_mean,
+                             levels=levels)
         else:
-            im = ax.contourf(infile_mean.coords['time'], infile_mean.coords[infile_mean.dims[-1]], 
-                         infile_mean.T,levels=levels)
+            im = ax.contourf(infile_mean.coords['time'],
+                             infile_mean.coords[infile_mean.dims[-1]],
+                             infile_mean.T, levels=levels)
     else:
         if invert_axis:
-            im = ax.pcolormesh(infile_mean.coords[infile_mean.dims[-1]], infile_mean.coords['time'], 
-                               infile_mean)
+            im = ax.pcolormesh(infile_mean.coords[infile_mean.dims[-1]],
+                               infile_mean.coords['time'], infile_mean)
         else:
-            im = ax.pcolormesh(infile_mean.coords['time'], infile_mean.coords[infile_mean.dims[-1]], infile_mean.T)
-        
+            im = ax.pcolormesh(infile_mean.coords['time'],
+                               infile_mean.coords[infile_mean.dims[-1]],
+                               infile_mean.T)
+
     # Colorbar
     try:
         cbar_label = infile_mean.name
@@ -154,46 +169,44 @@ def hovmoller_plot(infile,dim='lon',title=None, xlabel=None,ylabel=None,
         plt.colorbar(im, ax=ax, label=cbar_label)
     except AttributeError:
         plt.colorbar(im, ax=ax, label=infile_mean.name)
-    
-    set_layout(fig,ax,title=title,xlabel=xlabel,ylabel=ylabel,
-               xlog=xlog,ylog=ylog,xlim=xlim,ylim=ylim)
-    
+
+    set_layout(fig, ax, xlabel=xlabel, ylabel=ylabel,
+               title=title, **kwargs)
+
     # Custom labels if provided
-    if xlabel == None:
+    if xlabel is None:
         if invert_axis:
             ax.set_xlabel(infile_mean.dims[-1])
         else:
             ax.set_xlabel('time')
-    if ylabel == None:
+    if ylabel is None:
         if invert_axis:
             ax.set_ylabel('time')
         else:
             ax.set_ylabel(infile_mean.dims[-1])
-    if title == None:
+    if title is None:
         ax.set_title(f'Hovmoller Plot ({dim} mean)')
     if save:
         fig.savefig(outputdir + filename)
-    
+
     return fig, ax
 
-def index_plot(indx,title=None,xlabel=None,ylabel=None,xlog=False,
-               ylog=False,save=False,outputdir='./',filename='index.png'):
+
+def index_plot(indx, save=False, outputdir='./', filename='index.png',
+               **kwargs):
     """
     Index plot together with a black line at indx=0
 
     Args:
         indx (DataArray): Index DataArray
-        title (str,opt):        title of the plot
-        xlabel (str,opt):       label of the x axis
-        ylabel (str,opt):       label of the y axis
-        xlog (bool,opt):        enable or disable x axis log scale, 
-                                default is False
-        ylog (bool,opt):        enable or disable y axis log scale, 
-                                default is False
         save (bool,opt):        enable or disable saving the plot,
                                 default is False
-        outputdir (str,opt):    directory to save the plot
-    
+        outputdir (str,opt):    directory to save the plot,
+                                default is './'
+        filename (str,opt):     filename for the plot
+                                default is 'index.png'
+        **kwargs:               additional arguments for set_layout
+
     Returns:
         fig (Figure):           Figure object
         ax (Axes):              Axes object
@@ -202,16 +215,17 @@ def index_plot(indx,title=None,xlabel=None,ylabel=None,xlog=False,
     fig, ax = plt.subplots(figsize=(12, 8))
 
     # 2. -- Plot the index --
-    plt.fill_between(indx.time,indx.values,where=indx.values>=0, step="pre", 
-                     alpha=0.6,color='red')
-    plt.fill_between(indx.time,indx.values,where=indx.values<0, step="pre",
-                     alpha=0.6,color='blue')
-    indx.plot.step(ax=ax,color='black',alpha=0.8)
+    plt.fill_between(indx.time, indx.values, where=indx.values >= 0,
+                     step="pre", alpha=0.6, color='red')
+    plt.fill_between(indx.time, indx.values, where=indx.values < 0,
+                     step="pre", alpha=0.6, color='blue')
+    indx.plot.step(ax=ax, color='black', alpha=0.8)
 
-    ax.hlines(y=0,xmin=min(indx['time']),xmax=max(indx['time']),color='black')
+    ax.hlines(y=0, xmin=min(indx['time']), xmax=max(indx['time']),
+              color='black')
 
     # 3. -- Set the layout --
-    set_layout(fig,ax,title=title,xlabel=xlabel,ylabel=ylabel,xlog=xlog,ylog=ylog)
+    set_layout(fig, ax, **kwargs)
 
     # 4. -- Save the figure --
     if save:
@@ -219,31 +233,31 @@ def index_plot(indx,title=None,xlabel=None,ylabel=None,xlog=False,
 
     return fig, ax
 
-def reg_plot(indx,field,plot=True,projection_type='PlateCarree',
-             title=None,xlabel=None,ylabel=None,xlog=False,ylog=False,
-             contour=False,levels=8,save=False,outputdir='./',filename='reg.png'):
+
+def reg_plot(indx, field, plot=True, projection_type='PlateCarree',
+             contour=False, levels=8, save=False, outputdir='./',
+             filename='reg.png', **kwargs):
     """
-    Evaluate and plot regression map of a teleconnection index 
+    Evaluate and plot regression map of a teleconnection index
     and a DataArray field
 
     Args:
         indx (DataArray):       index DataArray
         field (DataArray):      field DataArray
+        plot (bool):            enable or disable the plot output,
+                                True by default
         projection_type (str):  projection style for cartopy
                                 If a wrong one is provided, it will fall back
                                 to PlateCarree
-        plot (bool):            enable or disable the plot output, true by default
-        title (str,opt):        title of the plot
-        xlabel (str,opt):       label of the x axis
-        ylabel (str,opt):       label of the y axis
-        xlog (bool,opt):        enable or disable x axis log scale, default is False
-        ylog (bool,opt):        enable or disable y axis log scale, default is False
-        contour (bool,opt):     enable or disable contour plot, default is False
+        contour (bool,opt):     enable or disable contour plot,
+                                default is False
         levels (int,opt):       number of contour levels, default is 8
-        save (bool,opt):        enable or disable saving the plot, default is False
+        save (bool,opt):        enable or disable saving the plot,
+                                default is False
         outputdir (str,opt):    directory to save the plot
         filename (str,opt):     filename of the plot
-    
+        **kwargs:               additional arguments for set_layout
+
     Returns:
         reg (DataArray):        DataArray for regression map
         fig (Figure,opt):       Figure object
@@ -257,22 +271,24 @@ def reg_plot(indx,field,plot=True,projection_type='PlateCarree',
     }
 
     # 2. -- Evaluate the regression --
-    reg = xr.cov(indx, field, dim="time")/indx.var(dim='time',skipna=True).values
-    
+    reg = xr.cov(indx, field, dim="time")/indx.var(dim='time',
+                                                   skipna=True).values
+
     # 3. -- Plot the regression map --
     proj = projection_types.get(projection_type, ccrs.PlateCarree())
 
     if plot:
-        fig, ax = plt.subplots(subplot_kw={'projection': proj},figsize=(8,4))
-        
+        fig, ax = plt.subplots(subplot_kw={'projection': proj}, figsize=(8, 4))
+
         ax.coastlines()
         if contour:
-            reg.plot.contourf(ax=ax, transform=ccrs.PlateCarree(),levels=levels)
+            reg.plot.contourf(ax=ax, transform=ccrs.PlateCarree(),
+                              levels=levels)
         else:
             reg.plot(ax=ax, transform=ccrs.PlateCarree())
-        
-        set_layout(fig, ax, title=title, xlabel=xlabel, ylabel=ylabel, xlog=xlog, ylog=ylog)
-        
+
+        set_layout(fig, ax, **kwargs)
+
         # 4. -- Save the figure --
         if save:
             fig.savefig(outputdir + filename)
@@ -281,19 +297,14 @@ def reg_plot(indx,field,plot=True,projection_type='PlateCarree',
     else:
         return reg
 
-def simple_plot(field,title=None,xlabel=None,ylabel=None,xlog=False,ylog=False,
-                save=False,outputdir='./',filename='plot.png'):
+
+def simple_plot(field, save=False, outputdir='./', filename='plot.png',
+                **kwargs):
     """
     Simple plot of a DataArray field
 
     Args:
         field (DataArray):      field DataArray
-        title (str,opt):        title of the plot
-        xlabel (str,opt):       label of the x axis
-        ylabel (str,opt):       label of the y axis
-        xlog (bool,opt):        enable or disable x axis log scale, default is False
-        ylog (bool,opt):        enable or disable y axis log scale, default is False
-        save (bool,opt):        enable or disable saving the plot, default is False
         outputdir (str,opt):    directory to save the plot
         filename (str,opt):     filename of the plot
 
@@ -308,10 +319,10 @@ def simple_plot(field,title=None,xlabel=None,ylabel=None,xlog=False,ylog=False,
     field.plot(ax=ax)
 
     # 3. -- Set the layout --
-    set_layout(fig,ax,title=title,xlabel=xlabel,ylabel=ylabel,xlog=xlog,ylog=ylog)
+    set_layout(fig, ax, **kwargs)
 
     # 4. -- Save the figure --
     if save:
         fig.savefig(outputdir + filename)
-    
+
     return fig, ax
--- diagnostics/teleconnections/tools.py
+++ diagnostics/teleconnections/tools.py
@@ -1,67 +1,77 @@
 '''
 This module contains simple tools for the teleconnections diagnostic.
+- area selection functions, to deal with custom areas
 - loading functions, to deal with yaml files
-- conversion functions, to deal with conversion between different physical units.
+- conversion functions, to deal with conversion between different
+  physical units.
+- weighted area mean function, to deal with weighted area mean
 '''
 import numpy as np
-from aqua import util
+from aqua.util import load_yaml
 
-def area_selection(indat,lat=None,lon=None,box_brd=True):
-    """ 
+
+def area_selection(indat, lat=None, lon=None, box_brd=True):
+    """
         Extract a custom area from a DataArray.
-    
+
         Args:
             indat (DataArray):  input data to be selected
             lat (list,opt):     latitude coordinates
             lon (list,opt):     longitude coordinates
             box_brd (bool,opt): choose if coordinates are comprised or not.
                                 Default is True
-    
+
         Returns:
             odat (DataArray): data on a custom surface
     """
     # 1. -- Extract coordinates from indat --
     if lat:
-        lat_coord=indat.lat
+        lat_coord = indat.lat
     if lon:
-        lon_coord=indat.lon
-    
+        lon_coord = indat.lon
+
     # 2. -- Select area --
     if box_brd:
         if lat:
-            iplat = lat_coord.where( (lat_coord >= lat[0] ) & (lat_coord <= lat[1]), drop=True)
+            iplat = lat_coord.where((lat_coord >= lat[0]) &
+                                    (lat_coord <= lat[1]), drop=True)
         if lon:
-            iplon = lon_coord.where( (lon_coord >= lon[0] ) & (lon_coord <= lon[1]), drop=True)
+            iplon = lon_coord.where((lon_coord >= lon[0]) &
+                                    (lon_coord <= lon[1]), drop=True)
     else:
         if lat:
-            iplat = lat_coord.where( (lat_coord > lat[0] ) & (lat_coord < lat[1]), drop=True)
+            iplat = lat_coord.where((lat_coord > lat[0]) &
+                                    (lat_coord < lat[1]), drop=True)
         if lon:
-            iplon = lon_coord.where( (lon_coord > lon[0] ) & (lon_coord < lon[1]), drop=True)
-    
+            iplon = lon_coord.where((lon_coord > lon[0]) &
+                                    (lon_coord < lon[1]), drop=True)
+
     # 3. -- Are selection --
-    odat=indat
+    odat = indat
     if lat:
-        odat=odat.sel(lat=iplat)
+        odat = odat.sel(lat=iplat)
     if lon:
-        odat=odat.sel(lon=iplon)
-    
+        odat = odat.sel(lon=iplon)
+
     return odat
 
-def load_namelist(diagname='teleconnections',configdir='./'):
+
+def load_namelist(diagname='teleconnections', configdir='./'):
     """
     Load diagnostic yaml file.
 
     Args:
         diagname (str):  diagnostic name
         configdir (str): path to config directory
-    
+
     Returns:
         namelist (dict): diagnostic config
     """
-    infile = f'{configdir}/{diagname}.yml'
-    namelist = util.load_yaml(infile)
-    
+    infile = f'{configdir}/{diagname}.yaml'
+    namelist = load_yaml(infile)
+
     return namelist
+
 
 def lon_180_to_360(lon):
     """
@@ -69,13 +79,14 @@ def lon_180_to_360(lon):
 
     Args:
         lon (float): longitude coordinate
-    
+
     Returns:
         lon (float): converted longitude
     """
-    if lon<0:
+    if lon < 0:
         lon = 360 + lon
     return lon
+
 
 def lon_360_to_180(lon):
     """
@@ -83,16 +94,18 @@ def lon_360_to_180(lon):
 
     Args:
         lon (float): longitude coordinate
-    
+
     Returns:
         lon (float): converted longitude
     """
-    if lon>180:
+    if lon > 180:
         lon = - 360 + lon
     return lon
 
-def wgt_area_mean(indat,latN:float,latS:float,lonW:float,lonE:float,box_brd=True):
-    """ 
+
+def wgt_area_mean(indat, latN: float, latS: float,
+                  lonW: float, lonE: float, box_brd=True):
+    """
     Evaluate the weighted mean of a quantity on a custom surface.
 
     Args:
@@ -108,14 +121,14 @@ def wgt_area_mean(indat,latN:float,latS:float,lonW:float,lonE:float,box_brd=True
         odat (DataArray): average of input data on a custom surface
     """
     # 1. -- Extract coordinates from indat --
-    lat=indat.lat
-    lon=indat.lon
+    lat = indat.lat
 
     # 2. -- Select area --
-    indat = area_selection(indat,lat=[latS,latN],lon=[lonW,lonE],box_brd=box_brd)
+    indat = area_selection(indat, lat=[latS, latN],
+                           lon=[lonW, lonE], box_brd=box_brd)
 
     # 3. -- Weighted area mean --
     wgt = np.cos(np.deg2rad(lat))
-    odat=indat.weighted(wgt).mean(("lon", "lat"), skipna=True)
+    odat = indat.weighted(wgt).mean(("lon", "lat"), skipna=True)
 
     return odat
