6e5433f85f7216ca9128748d15411ff62fba015f
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-04-06 11:13:29 +0200

    linting

--- tests/teleconnections/test_teleconnections.py
+++ tests/teleconnections/test_teleconnections.py
@@ -1,23 +1,25 @@
-import numpy as np
 import pytest
 import sys
-from aqua import Reader
 sys.path.insert(1, './diagnostics/teleconnections/')
-from cdotesting import cdo_station_based_comparison, cdo_regional_mean_comparison
+from cdotesting import cdo_station_based_comparison
+from cdotesting import cdo_regional_mean_comparison
 from tools import load_namelist, lon_180_to_360
 
-# pytest approximation, to bear with different machines
-approx_rel=1e4
 
-@pytest.mark.parametrize("module_name", ['cdotesting', 'index', 'plots', 'tools'])
+# pytest approximation, to bear with different machines
+approx_rel = 1e4
+
+
+@pytest.mark.parametrize("module_name", ['cdotesting', 'index', 'plots',
+                                         'tools'])
 def test_import(module_name):
     """
-    Test that the module can be imported
     """
     try:
         __import__(module_name)
     except ImportError:
         assert False, "Module {} could not be imported".format(module_name)
+
 
 def test_lon_conversion():
     """
@@ -25,54 +27,57 @@ def test_lon_conversion():
     """
     assert lon_180_to_360(-25) == pytest.approx(335, rel=approx_rel)
     assert lon_180_to_360(-75) == pytest.approx(285, rel=approx_rel)
-    assert lon_180_to_360(25)  == pytest.approx(25, rel=approx_rel)
-    assert lon_180_to_360(75)  == pytest.approx(75, rel=approx_rel)
+    assert lon_180_to_360(25) == pytest.approx(25, rel=approx_rel)
+    assert lon_180_to_360(75) == pytest.approx(75, rel=approx_rel)
+
 
 def test_namelist():
     """
     Test that the namelist can be loaded
     """
     configdir = "./diagnostics/teleconnections/"
-    diagname  = 'teleconnections'
-    namelist = load_namelist(diagname,configdir=configdir)
+    diagname = 'teleconnections'
+    namelist = load_namelist(diagname, configdir=configdir)
     assert len(namelist) > 0
 
-@pytest.mark.parametrize("months_window", [1,3])
+
+@pytest.mark.parametrize("months_window", [1, 3])
 def test_station_based(months_window):
     """
     Test that the station_based method works
     """
     filepath = "./nao_test.nc"
     configdir = "./diagnostics/teleconnections/"
-    diagname  = 'teleconnections'
+    diagname = 'teleconnections'
     telecname = 'NAO'
     rtol = 1e-4
     atol = 1e-4
 
     # 1. -- Opening yml files
-    namelist = load_namelist(diagname,configdir=configdir)
+    namelist = load_namelist(diagname, configdir=configdir)
 
     # 2. -- Comparison cdo vs lib method
-    cdo_station_based_comparison(infile=filepath,namelist=namelist,
-                                 telecname=telecname,rtol=rtol,atol=atol,
+    cdo_station_based_comparison(infile=filepath, namelist=namelist,
+                                 telecname=telecname, rtol=rtol, atol=atol,
                                  months_window=months_window)
 
-@pytest.mark.parametrize("months_window", [1,3])
+
+@pytest.mark.parametrize("months_window", [1, 3])
 def test_regional_mean(months_window):
     """
     Test that the regional_mean method works
     """
     filepath = "./enso_test.nc"
     configdir = "./diagnostics/teleconnections/"
-    diagname  = 'teleconnections'
+    diagname = 'teleconnections'
     telecname = 'ENSO'
     rtol = 1e-4
     atol = 1e-4
 
     # 1. -- Opening yml files
-    namelist = load_namelist(diagname,configdir=configdir)
+    namelist = load_namelist(diagname, configdir=configdir)
 
     # 2. -- Comparison cdo vs lib method
-    cdo_regional_mean_comparison(infile=filepath,namelist=namelist,
-                                 telecname=telecname,rtol=rtol,atol=atol,
+    cdo_regional_mean_comparison(infile=filepath, namelist=namelist,
+                                 telecname=telecname, rtol=rtol, atol=atol,
                                  months_window=months_window)
