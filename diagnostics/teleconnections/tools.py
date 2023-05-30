8e6e7c76891553e7d262781bdc3ad778e8abcec4
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-05-30 12:18:15 +0200

    improved error handling

--- diagnostics/teleconnections/tools.py
+++ diagnostics/teleconnections/tools.py
@@ -7,7 +7,7 @@ This module contains simple tools for the teleconnections diagnostic.
 - weighted area mean function, to deal with weighted area mean
 '''
 import numpy as np
-from aqua.util import load_yaml
+from aqua.util import load_yaml, get_config_dir
 
 
 def area_selection(indat, lat=None, lon=None, box_brd=True):
@@ -15,20 +15,29 @@ def area_selection(indat, lat=None, lon=None, box_brd=True):
         Extract a custom area from a DataArray.
 
         Args:
-            indat (DataArray):  input data to be selected
-            lat (list,opt):     latitude coordinates
-            lon (list,opt):     longitude coordinates
-            box_brd (bool,opt): choose if coordinates are comprised or not.
-                                Default is True
+            indat (DataArray):      input data to be selected
+            lat (list, opt):        latitude coordinates
+            lon (list, opt):        longitude coordinates
+            box_brd (bool,opt):     choose if coordinates are
+                                    comprised or not.
+                                    Default is True
 
         Returns:
-            odat (DataArray): data on a custom surface
+            odat (DataArray):       data on a custom surface
     """
     # 1. -- Extract coordinates from indat --
+    if lat is None and lon is None:
+        raise ValueError('lat and lon cannot be both None')
     if lat:
-        lat_coord = indat.lat
+        if lat[0] > lat[1]:
+            raise ValueError('lat must be specified in ascending order')
+        else:
+            lat_coord = indat.lat
     if lon:
-        lon_coord = indat.lon
+        if lon[0] > lon[1]:
+            raise ValueError('lon must be specified in ascending order')
+        else:
+            lon_coord = indat.lon
 
     # 2. -- Select area --
     if box_brd:
@@ -67,6 +76,9 @@ def load_namelist(diagname='teleconnections', configdir='./'):
     Returns:
         namelist (dict): diagnostic config
     """
+    if configdir is None:
+        configdir = get_config_dir()
+
     infile = f'{configdir}/{diagname}.yaml'
     namelist = load_yaml(infile)
 
