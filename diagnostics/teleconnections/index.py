b2fb6ff66d5b3a1cad11873a968f970dc88768f5
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-05-30 17:32:19 +0200

    index with logging + NAO notebooks

--- diagnostics/teleconnections/index.py
+++ diagnostics/teleconnections/index.py
@@ -2,10 +2,12 @@
 This module contains functions to evaluate teleconnection indices
 for different teleconnections.
 '''
+from aqua.logger import log_configure
 from tools import lon_180_to_360, wgt_area_mean
 
 
-def station_based_index(field, namelist, telecname, months_window=3):
+def station_based_index(field, namelist, telecname, months_window=3,
+                        loglevel='WARNING'):
     """
     Evaluate station based index for a teleconnection.
 
@@ -14,19 +16,34 @@ def station_based_index(field, namelist, telecname, months_window=3):
         namelist:                 teleconnection yaml infos
         telecname (str):          name of the teleconnection to be evaluated
         months_window (int, opt): months for rolling average, default is 3
+        loglevel (str, opt):      log level, default is WARNING
 
     Returns:
         indx (DataArray): standardized station based index
     """
+    # 0. -- Logging --
+    logger = log_configure(loglevel, 'station based index')
+    logger.info('Evaluating station based index for %s', telecname)
+
     # 1. -- Monthly field average and anomalies--
     field_av = field.groupby("time.month").mean(dim="time")
     field_an = field.groupby("time.month") - field_av
 
     # 2. -- Acquiring latitude and longitude of stations --
-    lon1 = lon_180_to_360(namelist[telecname]['lon1'])
+    if field.lon.min() < 0:
+        logger.info('Data longitudes are -180-180, not converting')
+        lon1 = namelist[telecname]['lon1']
+        lon2 = namelist[telecname]['lon2']
+    else:
+        logger.info('Data longitudes are 0-360, converting teleconnection coords')
+        lon1 = lon_180_to_360(namelist[telecname]['lon1'])
+        lon2 = lon_180_to_360(namelist[telecname]['lon2'])
+
     lat1 = namelist[telecname]['lat1']
-    lon2 = lon_180_to_360(namelist[telecname]['lon2'])
     lat2 = namelist[telecname]['lat2']
+
+    logger.info('Station 1: lon = %s, lat = %s', lon1, lat1)
+    logger.info('Station 2: lon = %s, lat = %s', lon2, lat2)
 
     # 3. -- Extracting field data at the acquired coordinates --
     field_an1 = field_an.sel(lon=lon1, lat=lat1, method='nearest')
@@ -49,10 +66,13 @@ def station_based_index(field, namelist, telecname, months_window=3):
     # 7. -- Drop NaNs --
     indx = indx.dropna(dim='time')
 
+    logger.info('Index evaluated')
+
     return indx
 
 
-def regional_mean_index(field, namelist, telecname, months_window=3):
+def regional_mean_index(field, namelist, telecname, months_window=3,
+                        loglevel='WARNING'):
     """
     Evaluate regional field mean for a teleconnection.
 
@@ -61,13 +81,25 @@ def regional_mean_index(field, namelist, telecname, months_window=3):
         namelist:                 teleconnection yaml infos
         telecname (str):          name of the teleconnection to be evaluated
         months_window (int, opt): months for rolling average, default is 3
+        loglevel (str, opt):      log level, default is WARNING
 
     Returns:
         field mean (DataArray): field mean
     """
+    # 0. -- Logging --
+    logger = log_configure(loglevel, 'regional mean index')
+    logger.info('Evaluating regional mean index for %s', telecname)
+
     # 1. -- Acquire coordinates --
-    lonW = lon_180_to_360(namelist[telecname]['lonW'])
-    lonE = lon_180_to_360(namelist[telecname]['lonE'])
+    if field.lon.min() < 0:
+        logger.info('Data longitudes are -180-180, not converting')
+        lonW = namelist[telecname]['lonW']
+        lonE = namelist[telecname]['lonE']
+    else:
+        logger.info('Data longitudes are 0-360, converting teleconnection coords')
+        lonW = lon_180_to_360(namelist[telecname]['lonW'])
+        lonE = lon_180_to_360(namelist[telecname]['lonE'])
+
     latN = namelist[telecname]['latN']
     latS = namelist[telecname]['latS']
 
@@ -79,10 +111,13 @@ def regional_mean_index(field, namelist, telecname, months_window=3):
     # 7. -- Drop NaNs --
     field_mean = field_mean.dropna(dim='time')
 
+    logger.info('Index evaluated')
+
     return field_mean
 
 
-def regional_mean_anomalies(field, namelist, telecname, months_window=3):
+def regional_mean_anomalies(field, namelist, telecname, months_window=3,
+                            loglevel='WARNING'):
     """
     Evaluate regional field mean anomalies for a teleconnection.
 
@@ -91,13 +126,25 @@ def regional_mean_anomalies(field, namelist, telecname, months_window=3):
         namelist:                 teleconnection yaml infos
         telecname (str):          name of the teleconnection to be evaluated
         months_window (int, opt): months for rolling average, default is 3
+        loglevel (str, opt):      log level, default is WARNING
 
     Returns:
         field mean (DataArray): field mean
     """
+    # 0. -- Logging --
+    logger = log_configure(loglevel, 'regional mean anomalies')
+    logger.info('Evaluating regional mean anomalies for %s', telecname)
+
     # 1. -- Acquire coordinates --
-    lonW = lon_180_to_360(namelist[telecname]['lonW'])
-    lonE = lon_180_to_360(namelist[telecname]['lonE'])
+    if field.lon.min() < 0:
+        logger.info('Data longitudes are -180-180, not converting')
+        lonW = namelist[telecname]['lonW']
+        lonE = namelist[telecname]['lonE']
+    else:
+        logger.info('Data longitudes are 0-360, converting teleconnection coords')
+        lonW = lon_180_to_360(namelist[telecname]['lonW'])
+        lonE = lon_180_to_360(namelist[telecname]['lonE'])
+
     latN = namelist[telecname]['latN']
     latS = namelist[telecname]['latS']
 
@@ -107,5 +154,7 @@ def regional_mean_anomalies(field, namelist, telecname, months_window=3):
         field_mean.groupby("time.month").mean(dim="time")
     field_mean_an = field_mean_an.rolling(time=months_window,
                                           center=True).mean(skipna=True)
+
+    logger.info('Index evaluated')
 
     return field_mean_an
