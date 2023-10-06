3185b5d551115834841916333669cd8f84bcf257
Paolo Ghinassi <p.ghinassi@isac.cnr.it>
2023-10-06 18:07:57 +0300

    fdb-long var list

--- diagnostics/tropical_cyclones/detect_nodes.py
+++ diagnostics/tropical_cyclones/detect_nodes.py
@@ -37,7 +37,7 @@ class DetectNodes():
             self.logger.warning(f"processing time step {tstep}")
             self.readwrite_from_intake(tstep)
             self.run_detect_nodes(tstep)
-            #clean_files([self.tempest_filein])
+            clean_files([self.tempest_filein])
             self.read_lonlat_nodes()
             self.store_detect_nodes(tstep)
             toc = time()
@@ -148,7 +148,7 @@ class DetectNodes():
             f'--closedcontourcmd {tempest_dictionary["psl"]},200.0,5.5,0;_DIFF({tempest_dictionary["zg"]}(30000Pa),{tempest_dictionary["zg"]}(50000Pa)),-58.8,6.5,1.0 --mergedist 6.0 ' \
             f'--outputcmd {tempest_dictionary["psl"]},min,0;_VECMAG({tempest_dictionary["uas"]},{tempest_dictionary["vas"]}),max,2;{tempest_dictionary["orog"]},min,0" --latname {tempest_dictionary["lat"]} --lonname {tempest_dictionary["lon"]}'
 
-        subprocess.run(detect_string.split()) #stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        subprocess.run(detect_string.split(), stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
     def store_detect_nodes(self, timestep, write_fullres=True):
         """
