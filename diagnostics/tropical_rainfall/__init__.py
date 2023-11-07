866fb4c8a9f2cf72dd3be5d6b3a8bdb0b7d212a0
Natalia Nazarova <natalia.nazarova@polito.it>
2023-11-07 01:04:17 +0100

    MainClass is added to the init file.

--- diagnostics/tropical_rainfall/__init__.py
+++ diagnostics/tropical_rainfall/__init__.py
@@ -2,11 +2,12 @@
 
 from .src.tropical_rainfall_tools import ToolsClass
 from .src.tropical_rainfall_plots import PlottingClass
+from .src.tropical_rainfall_main import MainClass
 from .tropical_rainfall_class import Tropical_Rainfall
 
 __version__ = '0.0.1'
 
-__all__ = ['Tropical_Rainfall', 'PlottingClass',  'ToolsClass']
+__all__ = ['Tropical_Rainfall', 'PlottingClass', 'ToolsClass', 'MainClass']
 
 # Change log
 # 0.0.1: Initial version
