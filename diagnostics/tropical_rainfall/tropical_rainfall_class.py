644fd24aff7b312aec17819f4a072ba6a6e6aea6
Natalia Nazarova <natalia.nazarova@polito.it>
2023-11-07 15:43:03 +0100

    The import of methods in the diagnostic class is shortened.

--- diagnostics/tropical_rainfall/tropical_rainfall_class.py
+++ diagnostics/tropical_rainfall/tropical_rainfall_class.py
@@ -81,32 +81,20 @@ class Tropical_Rainfall:
         else:
             self.width_of_bin = width_of_bin
         self.main.width_of_bin = self.width_of_bin
+        self.import_methods()
         
-        # the most important for import
-        self.histogram = self.main.histogram
-        self.merge_list_of_histograms = self.main.merge_list_of_histograms
-        self.histogram_plot = self.main.histogram_plot
-        self.average_into_netcdf = self.main.average_into_netcdf
-        self.plot_of_average = self.main.plot_of_average
-        self.plot_bias = self.main.plot_bias
-        self.plot_seasons_or_months = self.main.plot_seasons_or_months
-        self.seasonal_or_monthly_mean = self.main.seasonal_or_monthly_mean
-        self.map = self.main.map
-        self.get_95percent_level = self.main.get_95percent_level
-        self.seasonal_095level_into_netcdf = self.main.seasonal_095level_into_netcdf
-        self.add_UTC_DataAaray = self.main.add_UTC_DataAaray
-        self.daily_variability_plot = self.main.daily_variability_plot
-
-        # the less iportant for import 
-        self.histogram_lowres = self.main.histogram_lowres
-        self.merge_two_datasets =self.main.merge_two_datasets
-        self.add_frequency_and_pdf = self.main.add_frequency_and_pdf
-        self.precipitation_rate_units_converter = self.main.precipitation_rate_units_converter
-        self.preprocessing = self.main.preprocessing
-
-        # for test change the test
-        self.dataset_to_netcdf = self.main.dataset_to_netcdf
-        self.latitude_band = self.main.latitude_band
+    # the most important for import
+    def import_methods(self):
+        methods_to_import = [# more important for import
+                            'histogram', 'merge_list_of_histograms', 'histogram_plot', 'average_into_netcdf',
+                            'plot_of_average', 'plot_bias', 'plot_seasons_or_months', 'seasonal_or_monthly_mean',
+                            'map', 'get_95percent_level', 'seasonal_095level_into_netcdf', 'add_UTC_DataAaray',
+                            'daily_variability_plot', 
+                            # less important for import
+                            'histogram_lowres', 'merge_two_datasets', 'add_frequency_and_pdf', 'precipitation_rate_units_converter',
+                            'preprocessing']
+        for method_name in methods_to_import:
+            setattr(self, method_name, getattr(self.main, method_name))
 
 
     def class_attributes_update(self, **kwargs): 
@@ -123,4 +111,25 @@ class Tropical_Rainfall:
                 #self.main.class_attributes_update(getattr(attr_name)=getattr(attr_name)) #need to test the types 
             elif attr_name in kwargs and not isinstance(kwargs[attr_name], type(getattr(self, attr_name))):
                 raise TypeError(f"{attr_name} must be {type(getattr(self, attr_name))}")
+            
 Tropical_Rainfall.class_attributes_update.__doc__ = MainClass.class_attributes_update.__doc__
+"""
+Tropical_Rainfall.histogram.__doc__ = MainClass.histogram.__doc__
+Tropical_Rainfall.merge_list_of_histograms.__doc__ = MainClass.merge_list_of_histograms.__doc__
+Tropical_Rainfall.histogram_plot.__doc__ = MainClass.histogram_plot.__doc__
+Tropical_Rainfall.average_into_netcdf.__doc__ = MainClass.average_into_netcdf.__doc__
+Tropical_Rainfall.plot_of_average.__doc__ = MainClass.plot_of_average.__doc__
+Tropical_Rainfall.plot_bias.__doc__ = MainClass.plot_bias.__doc__
+Tropical_Rainfall.plot_seasons_or_months.__doc__ = MainClass.plot_seasons_or_months.__doc__
+Tropical_Rainfall.seasonal_or_monthly_mean.__doc__ = MainClass.seasonal_or_monthly_mean.__doc__
+Tropical_Rainfall.map.__doc__ = MainClass.map.__doc__
+Tropical_Rainfall.get_95percent_level.__doc__ = MainClass.get_95percent_level.__doc__
+Tropical_Rainfall.seasonal_095level_into_netcdf.__doc__ = MainClass.seasonal_095level_into_netcdf.__doc__
+Tropical_Rainfall.add_UTC_DataAaray.__doc__ = MainClass.add_UTC_DataAaray.__doc__
+Tropical_Rainfall.daily_variability_plot.__doc__ = MainClass.daily_variability_plot.__doc__
+Tropical_Rainfall.histogram_lowres.__doc__ = MainClass.histogram_lowres.__doc__
+Tropical_Rainfall.merge_two_datasets.__doc__ = MainClass.merge_two_datasets.__doc__
+Tropical_Rainfall.add_frequency_and_pdf.__doc__ = MainClass.add_frequency_and_pdf.__doc__
+Tropical_Rainfall.precipitation_rate_units_converter.__doc__ = MainClass.precipitation_rate_units_converter.__doc__
+Tropical_Rainfall.preprocessing.__doc__ = MainClass.preprocessing.__doc__
+"""
