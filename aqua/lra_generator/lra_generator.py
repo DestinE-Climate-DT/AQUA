5e00c21dce687afae167d2fde528dd7b109f50b9
silviacaprioli <silvietta.caprioli@gmail.com>
2023-11-23 11:23:53 +0100

    forgot comma

--- aqua/lra_generator/lra_generator.py
+++ aqua/lra_generator/lra_generator.py
@@ -234,7 +234,7 @@ class LRAgenerator():
                 'chunks': {},
                 'xarray_kwargs': {
                     'decode_times': True
-                }
+                },
             'metadata':
                 'source_grid_name': 'lon-lat'
             }
