41b8b24503a27225236bed120f723dba43737db5
Natalia Nazarova <natalia.nazarova@polito.it>
2023-11-24 04:39:15 +0100

    The old slurm script is renamed, and the new slurm script is added to the __init__ file.

--- aqua/slurm/__init__.py
+++ aqua/slurm/__init__.py
@@ -1,3 +1,3 @@
-from .slurm import squeue, job, output_dir, scancel, max_resources_per_node
-
+from .slurm_dask import squeue, job, output_dir, scancel, max_resources_per_node
+from .slurm import job
 __all__ = ['squeue', 'job', 'output_dir', 'scancel', 'max_resources_per_node']
