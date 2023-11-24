119b9fefd4b8254e743c842b7c26966172ccde7d
Natalia Nazarova <natalia.nazarova@polito.it>
2023-11-24 04:09:01 +0100

    The function 'remove_file' is added.

--- aqua/slurm/slurm.py
+++ aqua/slurm/slurm.py
@@ -16,8 +16,6 @@ def get_script_info(file_path=__file__):
     print("Script Path (external):", script_path)
     
     return script_name, script_path
-# Example usage
-#script_name, script_path = get_script_info()
 
 def extract_function_and_imports(source_code, function_name):
     tree = ast.parse(source_code)
@@ -40,7 +38,7 @@ def extract_function_and_imports(source_code, function_name):
 
     return imports, '\n'.join(extracted_code)
 
-def extract_and_write_function(source_path=__file__, function_name=None, destination_path=None):
+def extract_and_write_function(source_path=__file__, function_name=None):
     
     script_name, script_path = get_script_info()
     destination_path = script_path+'tmp_'+script_name
@@ -59,9 +57,8 @@ def extract_and_write_function(source_path=__file__, function_name=None, destina
             for imp in imports:
                 destination_file.write(f"{imp}\n")
             destination_file.write(f"{extracted_code}")
-# Example usage
-#extract_and_write_function("/work/bb1153/b382267/AQUA/cli/weights/generate_weights_for_catalog.py", "generate_catalogue_weights_on_slurm", "new_script.py")
-
+    return destination_path
+            
 def make_executable(file_path):
     """
     Make a Python file executable by adding the execute permission.
@@ -85,10 +82,27 @@ def make_executable(file_path):
         print(f"File '{file_path}' is now executable.")
     except Exception as e:
         print(f"Error making the file executable: {e}")
-# Example usage
-# make_executable("/work/bb1153/b382267/AQUA/aqua/slurm/new_script.py")
 
-def submit_slurm_job(script_path_func, job_name="my_job", output_file="my_job.out", error_file="my_job.err", time="24:00:00", nodes=1, tasks_per_node=1):
+def remove_file(file_path=None):
+    """
+    Remove a file.
+
+    Args:
+        file_path (str): Path to the file to be removed.
+
+    Returns:
+        bool: True if the file was successfully removed, False otherwise.
+    """
+    try:
+        os.remove(file_path)
+        print(f"File '{file_path}' removed successfully.")
+        return True
+    except Exception as e:
+        print(f"Error removing the file '{file_path}': {e}")
+        return False
+    
+def submit_slurm_job(script_path_func, job_name=None, output_file=None, error_file=None, 
+                     time=None, nodes=None, tasks_per_node=None):
     # Get the Python script path by calling the provided function
     python_script_path = script_path_func()
 
@@ -105,4 +119,12 @@ def submit_slurm_job(script_path_func, job_name="my_job", output_file="my_job.ou
 
     subprocess.run(slurm_command)
 
-# extract_and_write_function(source_path=__file__, function_name=None, destination_path=None)
+def job(source_path=__file__, function_name=None, job_name=None, output_file=None, 
+        error_file="my_job.err", time="24:00:00", nodes=1, tasks_per_node=1):
+
+    destination_path = extract_and_write_function(source_path=source_path, function_name=function_name) 
+    
+    
+    
+    
+    remove_file(file_path=destination_path)
