768b5998e72d47370cf1a13067fe3f605f3a7f24
mnurisso <m.nurisso@isac.cnr.it>
2024-03-04 16:42:08 +0000

    cftime attribute if needed

--- aqua/reader/reader.py
+++ aqua/reader/reader.py
@@ -548,6 +548,16 @@ class Reader(FixerMixin, RegridMixin, TimmeanMixin):
                 if not hasattr(data[var], 'units'):
                     self.logger.error('Variable %s has no units!', var)
 
+        # Check if the time axis is using cftime
+        if isinstance(data, xr.Dataset):
+            try:
+                if 'use_cftime' in self.esmcat.xarray_kwargs:
+                    cftime = self.esmcat.xarray_kwargs['use_cftime']
+            except AttributeError:
+                cftime = False
+            if cftime:
+                data = data.assign_attrs({'use_cftime': 1})
+
         if not fiter:  # This is not needed if we already have an iterator
             if self.streaming:
                 if self.stream_generator:
