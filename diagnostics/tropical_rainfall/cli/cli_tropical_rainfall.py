891d0850aa20ed623241660521af1b2d040c904b
Natalia Nazarova <natalia.nazarova@polito.it>
2024-06-19 05:31:20 +0300

    Updating `cli_tropical_rainfall` script.

--- diagnostics/tropical_rainfall/cli/cli_tropical_rainfall.py
+++ diagnostics/tropical_rainfall/cli/cli_tropical_rainfall.py
@@ -1,27 +1,36 @@
 import sys
 import os
 from aqua.util import get_arg
-aqua_path = os.getenv('AQUA')  # This will return None if 'AQUA' is not set
 from src.tropical_rainfall_utils import parse_arguments, validate_arguments, load_configuration
 from src.tropical_rainfall_cli_class import Tropical_Rainfall_CLI
+from tropical_rainfall import __path__ as pypath
 
 def main():
     """Main function to orchestrate the tropical rainfall CLI operations."""
+    try:
+        # Parse command line arguments
+        args = parse_arguments(sys.argv[1:])
 
-    args = parse_arguments(sys.argv[1:])
-    validate_arguments(args)
+        # Validate the parsed arguments
+        validate_arguments(args)
 
-    config_file = get_arg(args, 'config', f'{aqua_path}/diagnostics/tropical_rainfall/cli/cli_config_trop_rainfall.yml')
+        # Determine the configuration file path
+        config_file = get_arg(args, 'config', os.path.join(pypath[0], 'cli', 'cli_config_trop_rainfall.yml'))
 
-    config = load_configuration(config_file)
+        # Load the configuration from the file
+        config = load_configuration(config_file)
 
-    trop_rainfall_cli = Tropical_Rainfall_CLI(config, args)
-    trop_rainfall_cli.calculate_histogram_by_months()
-    trop_rainfall_cli.plot_histograms()
-    trop_rainfall_cli.daily_variability()
-    trop_rainfall_cli.plot_daily_variability()
-    trop_rainfall_cli.average_profiles()
+        # Initialize the Tropical Rainfall CLI with the loaded configuration and arguments
+        trop_rainfall_cli = Tropical_Rainfall_CLI(config, args)
+
+        # Uncomment and call the desired methods
+        trop_rainfall_cli.calculate_histogram_by_months()
+        trop_rainfall_cli.plot_histograms()
+        trop_rainfall_cli.average_profiles()
+
+    except Exception as e:
+        print(f"An error occurred: {e}", file=sys.stderr)
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
-
