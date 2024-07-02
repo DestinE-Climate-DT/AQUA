fe0ec2873e0697d0877100b0bd712f22fa507e37
silviacaprioli <silvietta.caprioli@gmail.com>
2024-07-02 18:43:48 +0300

    atmglobalmean+radiation refactoring

--- diagnostics/atmglobalmean/__init__.py
+++ diagnostics/atmglobalmean/__init__.py
@@ -1,6 +1,5 @@
 """Atmospheric global mean diagnostic module"""
-from .atm_global_mean import seasonal_bias, compare_datasets_plev, plot_map_with_stats
-
+from .atmglobalmean import AtmosphericGlobalMean
 __version__ = '0.0.1'
 
-__all__ = ['seasonal_bias', 'compare_datasets_plev', 'plot_map_with_stats']
+__all__ = ['AtmosphericGlobalMean']
--- diagnostics/atmglobalmean/cli/cli_atmglobalmean.py
+++ diagnostics/atmglobalmean/cli/cli_atmglobalmean.py
@@ -12,11 +12,12 @@ from dask.distributed import Client, LocalCluster
 def parse_arguments(args):
     """Parse command line arguments"""
 
-    parser = argparse.ArgumentParser(description='Atmospheric global mean biases CLI')
+    parser = argparse.ArgumentParser(description='Atmospheric global biases CLI')
     parser.add_argument('-c', '--config', type=str,
                         help='yaml configuration file')
     parser.add_argument('-n', '--nworkers', type=int,
                         help='number of dask distributed workers')
+
     # This arguments will override the configuration file if provided
     parser.add_argument('--model', type=str, help='model name',
                         required=False)
@@ -28,13 +29,13 @@ def parse_arguments(args):
                         required=False)
     parser.add_argument('--loglevel', '-l', type=str, help='loglevel',
                         required=False)
-    parser.add_argument('--start_date1', type=str, help='start date for dataset1',
+    parser.add_argument('--startdate_data', type=str, help='start date for dataset',
                         required=False)
-    parser.add_argument('--end_date1', type=str, help='end date for dataset1',
+    parser.add_argument('--enddate_data', type=str, help='end date for dataset',
                         required=False)
-    parser.add_argument('--start_date2', type=str, help='start date for dataset2',
+    parser.add_argument('--startdate_ref', type=str, help='start date for reference dataset',
                         required=False)
-    parser.add_argument('--end_date2', type=str, help='end date for dataset2',
+    parser.add_argument('--enddate_ref', type=str, help='end date for reference dataset',
                         required=False)
 
     return parser.parse_args(args)
@@ -57,7 +58,7 @@ if __name__ == '__main__':
 
     try:
         sys.path.insert(0, '../../')
-        from atmglobalmean import compare_datasets_plev, seasonal_bias, plot_map_with_stats
+        from atmglobalmean import AtmosphericGlobal
     except ImportError as import_error:
         # Handle ImportError
         logger.error(f"ImportError occurred: {import_error}")
@@ -79,21 +80,8 @@ if __name__ == '__main__':
         client = Client(cluster)
         logger.info(f"Running with {nworkers} dask distributed workers.")
 
-    # Acquiring model, experiment and source
-    model = get_arg(args, 'model', config['data']['model'])
-    exp = get_arg(args, 'exp', config['data']['exp'])
-    source = get_arg(args, 'source', config['data']['source'])
-
-    # Acquiring start and end dates for dataset1 and dataset2
-    start_date1 = get_arg(args, 'start_date1', None)
-    end_date1 = get_arg(args, 'end_date1', None)
-    start_date2 = get_arg(args, 'start_date2', None)
-    end_date2 = get_arg(args, 'end_date2', None)
-
-    logger.debug(f"Running for {model} {exp} {source}.")
-
     path_to_output = get_arg(
-        args, 'outputdir', config['path']['path_to_output'])
+    args, 'outputdir', config['path']['path_to_output'])
     if path_to_output:
         outputdir = os.path.join(path_to_output, 'netcdf/')
         outputfig = os.path.join(path_to_output, 'pdf/')
@@ -105,96 +93,61 @@ if __name__ == '__main__':
     logger.debug(f"outputdir: {outputdir}")
     logger.debug(f"outputfig: {outputfig}")
 
-    # This is the model to compare with
-    model_obs = config['data']['model_obs']
+    # Acquiring model, experiment and source
+    model = get_arg(args, 'model', config['data']['model'])
+    exp = get_arg(args, 'exp', config['data']['exp'])
+    source = get_arg(args, 'source', config['data']['source'])
+
+    # Acquiring model, experiment and source for reference data
+    model_obs= config['data']['model_obs']
     exp_obs = config['data']['exp_obs']
     source_obs = config['data']['source_obs']
 
+    logger.debug(f"Running for {model} {exp} {source}.")
+
     logger.debug(f"Comparing with {model_obs} {exp_obs} {source_obs}.")
 
-    variables_no_plev = config['diagnostic_attributes'].get('variables_no_plev', [])
-    variables_with_plev = config['diagnostic_attributes'].get('variables_with_plev', [])
+    variables= config['diagnostic_attributes'].get('variables', [])
     plev = config['diagnostic_attributes'].get('plev', None)
-    statistic = config['diagnostic_attributes'].get('statistic', 'mean')
-    seasonal_bias_bool = config['diagnostic_attributes'].get('seasonal_bias', True)
-    compare_datasets_plev_bool = config['diagnostic_attributes'].get('compare_datasets_plev', False)
-    plot_map_with_stats_bool = config['diagnostic_attributes'].get('plot_map_with_stats', False)
-    start_date1 = config['diagnostic_attributes'].get('start_date1', None)
-    end_date1 = config['diagnostic_attributes'].get('end_date1', None)
-    start_date2 = config['diagnostic_attributes'].get('start_date2', "1980-01-01")
-    end_date2 = config['diagnostic_attributes'].get('end_date2', "2010-12-31")
+    seasons_bool = config['diagnostic_attributes'].get('seasons', False)
+    seasons_stat = config['diagnostic_attributes'].get('seasons_stat', 'mean')
+    vertical = config['diagnostic_attributes'].get('vertical', False)
+    startdate_data = config['diagnostic_attributes'].get('startdate_data', None)
+    enddate_data = config['diagnostic_attributes'].get('enddate_data', None)
+    startdate_obs = config['diagnostic_attributes'].get('startdate_obs', "1980-01-01")
+    enddate_obs = config['diagnostic_attributes'].get('enddate_obs', "2010-12-31")
 
     model_label = model+'_'+exp
     model_label_obs = model_obs+'_'+exp_obs
 
     try:
-        reader_obs = Reader(model=model_obs, exp=exp_obs, source=source_obs, loglevel=loglevel)
-        data_obs = reader_obs.retrieve()
-    except Exception as e:
-        logger.error(f"No observation data found: {e}")
-
-    try:
-        reader = Reader(model=model, exp=exp, source=source, loglevel=loglevel)
+        reader = Reader(model=model, exp=exp, source=source, startdate=startdate_data, enddate=enddate_data, loglevel=loglevel)
         data = reader.retrieve()
     except Exception as e:
         logger.error(f"No model data found: {e}")
         logger.critical("Atmospheric global mean biases diagnostic is terminated.")
         sys.exit(0)
 
-    if seasonal_bias_bool:
-        for var_name in variables_no_plev:
-            logger.info(f"Running seasonal bias diagnostic for {var_name}...")
+    try:
+        reader_obs = Reader(model=model_obs, exp=exp_obs, source=source_obs, startdate=startdate_obs, enddate=enddate_obs, loglevel=loglevel)
+        data_obs = reader_obs.retrieve()
+    except Exception as e:
+        logger.error(f"No observation data found: {e}")
 
+    for var_name in variables:
+        logger.info(f"Running seasonal bias diagnostic for {var_name}...")
+
+        if vertical:
             # Getting variable specific attributes
-            var_attributes = config['seasonal_bias'].get(var_name, {})
-            vmin = var_attributes.get('vmin', None)
-            vmax = var_attributes.get('vmax', None)
-            seasons = config['diagnostic_attributes'].get('seasons', None)
-            logger.debug(f"var: {var_name}, vmin: {vmin}, vmax: {vmax}")
-
-            try:
-                seasonal_bias(dataset1=data, dataset2=data_obs,
-                              var_name=var_name, plev=plev, statistic=statistic,
-                              model_label1=model_label, model_label2=model_label_obs,
-                              start_date1=start_date1, end_date1=end_date1,
-                              start_date2=start_date2, end_date2=end_date2,
-                              outputdir=outputdir, outputfig=outputfig,
-                              vmin=vmin, vmax=vmax,
-                              loglevel=loglevel, seasons=seasons)
-            except Exception as e:
-                logger.error(f"An unexpected error occurred: {e}")
-
-    if compare_datasets_plev_bool:
-        for var_name in variables_with_plev:
-            logger.info(f"Running compare datasets plev diagnostic for {var_name}...")
-            
-            # Getting variable specific attributes
-            var_attributes = config['compare_datasets_plev'].get(var_name, {})
+            var_attributes = config['vertical_plev'].get(var_name, {})
             vmin = var_attributes.get('vmin', None)
             vmax = var_attributes.get('vmax', None)
             logger.debug(f"var: {var_name}, vmin: {vmin}, vmax: {vmax}")
-            plev_min = config['compare_datasets_plev'].get('plev_min', None)
-            plev_max = config['compare_datasets_plev'].get('plev_max', None)
-            
-            try:
-                compare_datasets_plev(dataset1=data, dataset2=data_obs, var_name=var_name,
-                                      model_label1=model_label, model_label2=model_label_obs,
-                                      start_date1=start_date1, end_date1=end_date1,
-                                      start_date2=start_date2, end_date2=end_date2,
-                                      outputdir=outputdir, outputfig=outputfig,
-                                      vmin=vmin, vmax=vmax,
-                                      plev_min=plev_min, plev_max=plev_max,
-                                      loglevel=loglevel)
-            except Exception as e:
-                logger.error(f"An unexpected error occurred: {e}")
 
-    if plot_map_with_stats_bool:
-        for var_name in variables_no_plev:
-            logger.info(f"Running plot map with stats diagnostic for {var_name}...")
-            try:
-                plot_map_with_stats(dataset=data, var_name=var_name,  model_label=model_label,
-                                    outputdir=outputdir, outputfig=outputfig, loglevel=loglevel)
-            except Exception as e:
-                logger.error(f"An unexpected error occurred: {e}")
+        try:
+            atmos_global = AtmosphericGlobal(data=data, data_ref = data_obs, var_name=var_name, seasons=seasons_bool, plev=plev,
+                                             vertical=vertical, vmin=vmin, vmax=vmax)
+        except Exception as e:
+            logger.error(f"An unexpected error occurred: {e}")
 
     logger.info("Atmospheric global mean biases diagnostic is terminated.")
