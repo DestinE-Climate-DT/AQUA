2ad09ea4bb81b47ebf57f40c4e77899687879e2c
maqsoodrajput <24833949+maqsoodrajput@users.noreply.github.com>
2024-10-02 16:17:14 +0200

    initial ensemble statistics
    right now it is basic structure of 1D timeseries multi model ensemble

---
+++ diagnostics/ensemble/_cli_timeseries_ensemble.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+"""
+Command-line interface for global time series diagnostic.
+
+This CLI allows to plot timeseries of a set of variables
+defined in a yaml configuration file for a single or multiple
+experiments and gregory plot.
+"""
+import argparse
+import os
+import sys
+from dask.distributed import Client, LocalCluster
+
+from aqua.util import load_yaml, get_arg
+from aqua.exceptions import NotEnoughDataError, NoDataError, NoObservationError
+from aqua.logger import log_configure
+#from global_time_series import Timeseries, GregoryPlot, SeasonalCycle
+
+from timeseries_ensemble import Ensemble_timeseries 
+
+
+def parse_arguments(args):
+    """Parse command line arguments."""
+
+    parser = argparse.ArgumentParser(description="Global time series CLI")
+
+    parser.add_argument("-c", "--config",
+                        type=str, required=False,
+                        help="yaml configuration file")
+    parser.add_argument('-n', '--nworkers', type=int,
+                        help='number of dask distributed workers')
+    parser.add_argument("--loglevel", "-l", type=str,
+                        required=False, help="loglevel")
+
+    # These will override the first one in the config file if provided
+    parser.add_argument("--catalog", type=str,
+                        required=False, help="catalog name")
+    parser.add_argument("--model", type=str,
+                        required=False, help="model name")
+    parser.add_argument("--exp", type=str,
+                        required=False, help="experiment name")
+    parser.add_argument("--source", type=str,
+                        required=False, help="source name")
+    parser.add_argument("--outputdir", type=str,
+                        required=False, help="output directory")
+
+    return parser.parse_args(args)
+
+def get_plot_options(config: dict = None, var: str = None):
+    plot_options = config["timeseries_plot_params"].get(var)
+    #regrid = plot_options.get("regrid", False)
+    startdate = config["timeseries_plot_params"]["default"].get("startdate", None)
+    enddate = config["timeseries_plot_params"]["default"].get("enddate", None)
+    plot_kw = config["timeseries_plot_params"]["default"].get("plot_kw", {})
+    units = None
+    return startdate,enddate,plot_kw,units
+
+if __name__ == '__main__':
+
+    args = parse_arguments(sys.argv[1:])
+
+    loglevel = get_arg(args, "loglevel", "WARNING")
+    logger = log_configure(loglevel, 'CLI Global Time Series')
+    logger.info("Running Global Time Series diagnostic")
+
+    # Moving to the current directory so that relative paths work
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    if os.getcwd() != dname:
+        os.chdir(dname)
+        logger.info(f"Changing directory to {dname}")
+
+    # Dask distributed cluster
+    nworkers = get_arg(args, 'nworkers', None)
+    if nworkers:
+        cluster = LocalCluster(n_workers=nworkers, threads_per_worker=1)
+        client = Client(cluster)
+        logger.info(f"Running with {nworkers} dask distributed workers.")
+
+    # Load configuration file
+    file = get_arg(args, "config", "config_timeseries_ensemble.yaml")
+    logger.info(f"Reading configuration file {file}")
+    config = load_yaml(file)
+
+    models = config['models']
+    models[0]['catalog'] = get_arg(args, 'catalog', models[0]['catalog'])
+    models[0]['model'] = get_arg(args, 'model', models[0]['model'])
+    models[0]['exp'] = get_arg(args, 'exp', models[0]['exp'])
+    models[0]['source'] = get_arg(args, 'source', models[0]['source'])
+
+    logger.debug("Analyzing models:")
+    catalogs_list = []
+    models_list = []
+    exps_list = []
+    sources_list = []
+
+    for model in models:
+        logger.debug(f"  - {model['catalog']} {model['model']} {model['exp']} {model['source']}")
+        catalogs_list.append(model['catalog'])
+        models_list.append(model['model'])
+        exps_list.append(model['exp'])
+        sources_list.append(model['source'])
+
+    outputdir = get_arg(args, "outputdir", config["outputdir"])
+
+    if "timeseries" in config:
+        var = config['timeseries']
+        logger.info(f"Plotting {var} timeseries")
+        startdate,enddate,plot_kw,units = get_plot_options(config,var)
+        print("here!!!!!! ",startdate, var)
+
+        #ts = Ensemble_timeseries(var=var,catalogs=catalogs_list,models=models_list,exps=exps_list,sources=sources_list,startdate=startdate,enddate=enddate,loglevel=loglevel)
+        ts = Ensemble_timeseries(var=var,models=models_list,exps=exps_list,sources=sources_list,startdate=startdate,enddate=enddate,loglevel=loglevel)
+
+        try:
+            ts.run()
+        except Exception as e:
+            logger.error(f'Error plotting {var} timeseries: {e}')
+
---
+++ diagnostics/ensemble/timeseries_ensemble.py
@@ -0,0 +1,164 @@
+import os
+import gc
+
+import xarray as xr 
+from aqua import Reader
+from aqua.logger import log_configure
+from aqua.exceptions import NoObservationError, NoDataError
+from aqua.util import eval_formula,create_folder
+from aqua.util import add_pdf_metadata, time_to_string
+from aqua.graphics import plot_timeseries
+import matplotlib.pyplot as plt
+
+#from .reference_data import get_referrnce_timeseries
+
+# 1- Compute the mean and std of the timeseries coming the aqua analysis
+# 2- plot the computed statistics 
+# 3- plots the individual timeseries
+# 4- plots the reference ERA5 timeseries from the aqua analysis
+
+xr.set_options(keep_attrs=True)
+
+
+class Ensemble_timeseries():
+    def __init__(self,var=None,models=None,exps=None,sources=None,startdate=None,enddate=None,plot_kw={'ylim':{}},longname=None,save=True,outdir='./',outfile=None,loglevel='WARNING'):
+    
+    #def __init__(self,var=None,catalogs=None,models=None,exps=None,sources=None,startdate=None,enddate=None,plot_kw={'ylim':{}},longname=None,save=True,outdir='./',outfile=None,loglevel='WARNING'):
+    
+
+        self.loglevel = loglevel
+        #self.formula = formula
+        self.logger = log_configure(log_level=self.loglevel, log_name='Timeseries')
+        self.var = var
+        self.models = models
+        self.exps = exps
+        self.sources= sources
+        #self._catalogs(catalogs=catalogs)
+        #self.catalogs = catalogs
+
+        if self.models is None or self.exps is None or self.sources is None:
+            raise NoDataError("No models, exps or sources provided")
+
+        #if isinstance(self.catalogs, str):
+        #    self.catalogs = [self.catalogs]
+        if isinstance(self.models,str):
+            self.models = [self.models]
+        if isinstance(self.exps,str):
+            self.exps = [self.exps]
+        if isinstance(self.sources,str):
+            self.sources = [self.sources]
+        #self.regrid = False
+        self.startdate = startdate
+        self.enddate = enddate
+
+        self.plot_kw = plot_kw
+        self.longname = longname
+        #self.units = units
+       
+        self.save = save
+
+        if self.save is False:
+            self.logger.info("Figure will not ne saved")
+
+        self.outdir = outdir
+        self.outfile = outfile
+        self.dataset_mean = None
+        self.dataset_std = None
+
+
+    def run(self):
+        self.retrieve_data()
+        self.plot()
+            
+
+    def retrieve_data(self):
+        self.logger.debug("Retrieving data")
+        self.dataset_mean = []
+        self.dataset_std = []
+        dataset = []
+        # You don't want to concatenate inside the loop -- that would make your code run in quadratic time: https://stackoverflow.com/questions/33435953/is-it-possible-to-append-to-an-xarray-dataseti
+
+        for i, model in enumerate(self.models):
+            #self.logger.info(f'Retrieving data for {self.catalogs[i]} {model} {self.exps[i]} {self.sources[i]}')
+            self.logger.info(f'Retrieving data for {model} {self.exps[i]} {self.sources[i]}')
+            i_model = model;i_exp=self.exps[i];i_source=self.sources[i];istart=self.startdate;iend=self.enddate;var=self.var
+            #print('Data Reader: ', i_model,' ',i_exp,' ',i_source,' ',istart,iend)
+            #try:
+            #    reader = Reader(model=i_model,exp=i_exp,source=i_source,startdate=istart,enddate=iend,areas=False)
+            #    #reader = Reader(model=model,exp=self.exps[i],source=self.sources[i],startdate=self.startdate,enddate=self.enddate,area=False)
+            #    data = reader.retrieve(self.var)
+            #    dataset.append(data)
+            #except Exception as e:
+            #    self.logger.debug(f'Error while retrieving: {e}')
+            #    #self.logger.warning(f'No data found for {self.catalog[i]} {model} {self.exps[i]} {self.sources[i]}')
+            #    self.logger.warning(f'No data found for {model} {self.exps[i]} {self.sources[i]}')
+        #combined_dataset = xr.concat(dataset,dim='Ensemble')
+        #self.dataset_mean = combined_dataset[self.var].mean(dim='Ensemble')
+        #self.dataset_std = combined_dataset[self.var].std(dim='Ensemble')
+
+            try:
+                reader = Reader(model=i_model,exp=i_exp,source=i_source,startdate=istart,enddate=iend,areas=False)
+                #reader = Reader(model=model,exp=self.exps[i],source=self.sources[i],startdate=self.startdate,enddate=self.enddate,area=False)
+                data = reader.retrieve(var)
+                dataset.append(data)
+            except Exception as e:
+                self.logger.debug(f'Error while retrieving: {e}')
+                #self.logger.warning(f'No data found for {self.catalog[i]} {model} {self.exps[i]} {self.sources[i]}')
+                self.logger.warning(f'No data found for {model} {self.exps[i]} {self.sources[i]}')
+        combined_dataset = xr.concat(dataset,dim='Ensemble')
+        self.dataset_mean = combined_dataset[var].mean(dim='Ensemble')
+        self.dataset_std = combined_dataset[var].std(dim='Ensemble')
+
+        # Clean up
+        del reader
+        del data
+        del dataset
+        del combined_dataset
+        gc.collect()
+        
+        #startdate = time_to_string(startdate)
+        #self.startdate = startdate
+        #self.logger.debug(f'Start date: {self.startdate}')
+        #enddate = time_to_string(startdate)
+        #self.enddate = enddate
+        #self.logger.debug('End date: {self.enddate}')
+
+        if self.dataset_mean.sizes == 0:
+            raise NoDataError('No data found')
+
+    def plot(self):
+        fig, ax = plt.subplots(1, 1)
+        self.logger.info('Plotting the timeseries')
+        data_labels = []
+        var = self.var[0]
+        data_mean = self.dataset_mean
+        data_mean = data_mean[var]
+        data_std = self.dataset_std
+        data_std = data_std[var]
+        #self.dataset_mean[self.var].plot(ax=ax)
+        data_mean.plot(ax=ax)
+        ax.fill_between(data_mean.time, data_mean -2.*data_std,data_mean +2.*data_std,facecolor='grey',alpha=0.25)
+        #data_mean.plot(ax=ax)
+        
+        ax.figure.savefig('ens.png')
+        #    return fig,ax
+
+    def save_pdf(self):
+        pass
+
+    def save_netcdf(self):
+        pass
+
+    def check_range(self):
+        pass
+
+    def _catalogs(self):
+        pass
+      
+    def reference_data(self):
+        pass
+
+    def cleanup(self):
+        pass
+
+
