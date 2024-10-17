e80d0a69fd9d59366e699611edad876aac32af30
maqsoodrajput <24833949+maqsoodrajput@users.noreply.github.com>
2024-10-18 00:44:30 +0200

    addition in ensemble atmglobalmean #2

--- diagnostics/ensemble/ensemble_atmglobalmean/atmglobalmean_ensemble.py
+++ diagnostics/ensemble/ensemble_atmglobalmean/atmglobalmean_ensemble.py
@@ -9,3 +9,21 @@ from aqua.util import create_folder
 from aqua.util import add_pdf_metadata,time_to_string
 from matplotlib.pyplot as plt
 xr.set_options(keep_attrs=True)
+
+class AtmglobalmeanEnsemble():
+    def __init__(self):
+        """
+        """
+        pass
+    
+    def retrieve_data(self):
+        pass
+
+    def plot(self):
+        pass
+
+    def save_pdf(self):
+        pass
+
+    def run(self):
+        pass
