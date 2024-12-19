081fa59a63c2951bb26ad4a29d3da3c2db6e74ef
mnurisso <m.nurisso@isac.cnr.it>
2024-12-19 10:53:32 +0200

    update options for hovmoller plot

--- src/aqua/graphics/hovmoller.py
+++ src/aqua/graphics/hovmoller.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
 
-from aqua.util import create_folder, evaluate_colorbar_limits
+from aqua.util import create_folder, evaluate_colorbar_limits, to_list
 from aqua.logger import log_configure
 
 # set default options for xarray
@@ -41,7 +41,7 @@ def plot_hovmoller(data: xr.DataArray,
         contour (bool,opt):     True for contour plot, False for pcolormesh,
                                 default is True
         save (bool,opt):        save the figure, default is False
-        dim (str,opt):          dimension to be averaged over,
+        dim (str|list,opt):     dimensions to be averaged over,
                                 default is 'lon'
         figsize (tuple,opt):    figure size, default is (11, 8.5)
         vmin (float,opt):       minimum value for the colorbar
@@ -63,24 +63,37 @@ def plot_hovmoller(data: xr.DataArray,
         format (str, optional):      Format of the figure. Defaults to 'pdf'.
         dpi (int, optional):         Dots per inch. Defaults to 100 for pcolormesh
                                      and 300 for contour plots.
+        xlog (bool, optional):       Use log scale for the x-axis. Defaults to False.
+        ylog (bool, optional):       Use log scale for the y-axis. Defaults to False.
+        xinvert (bool, optional):    Invert the x-axis. Defaults to False.
+        yinvert (bool, optional):    Invert the y-axis. Defaults to False.
 
     Returns:
         fig, ax: tuple with the figure and axes
     """
-    logger = log_configure(log_level=loglevel, log_name='Hovmoller')
+    logger = log_configure(log_level=loglevel, log_name='plot_hovmoller')
 
     # Check if data is a DataArray
     if not isinstance(data, xr.DataArray):
         raise TypeError('Data is not a DataArray')
 
     # Evaluate the mean over the dimension to be averaged over
-    logger.info('Averaging over dimension: {}'.format(dim))
-    dim_min = data.coords[dim].min()
-    dim_max = data.coords[dim].max()
-    dim_min = np.round(dim_min, 0)
-    dim_max = np.round(dim_max, 0)
+    dim = to_list(dim)
+    logger.info('Averaging over dimensions: {}'.format(dim))
+    label_text = ''
+    for d in dim:
+        dim_min = data.coords[d].min()
+        dim_max = data.coords[d].max()
+        dim_min = np.round(dim_min, 0)
+        dim_max = np.round(dim_max, 0)
     
-    data_mean = data.mean(dim=dim)
+        data = data.mean(dim=d)
+
+        label_text += f'{d} = {dim_min:.2f} to {dim_max:.2f} '
+    
+    # Check that we have a 2D array
+    if len(data.dims) != 2:
+        raise ValueError('DataArray is not 2D')
 
     # Create figure and axes
     fig, ax = plt.subplots(figsize=figsize)
@@ -88,17 +101,17 @@ def plot_hovmoller(data: xr.DataArray,
     # Plot the data
     if invert_axis:
         logger.debug('Inverting axis for plot')
-        x = data_mean.coords[data_mean.dims[-1]]
-        y = data_mean.coords['time']
-        ax.set_xlabel(data_mean.dims[-1])
+        x = data.coords[data.dims[-1]]
+        y = data.coords['time']
+        ax.set_xlabel(data.dims[-1])
         ax.set_ylabel('time')
         if invert_time:
             plt.gca().invert_yaxis()
     else:
-        x = data_mean.coords['time']
-        y = data_mean.coords[data_mean.dims[-1]]
+        x = data.coords['time']
+        y = data.coords[data.dims[-1]]
         ax.set_xlabel('time')
-        ax.set_ylabel(data_mean.dims[-1])
+        ax.set_ylabel(data.dims[-1])
         if invert_time:
             plt.gca().invert_xaxis()
 
@@ -114,25 +127,25 @@ def plot_hovmoller(data: xr.DataArray,
         try:
             levels = np.linspace(vmin, vmax, nlevels)
             if invert_axis:
-                im = ax.contourf(x, y, data_mean, levels=levels, cmap=cmap,
+                im = ax.contourf(x, y, data, levels=levels, cmap=cmap,
                                  vmin=vmin, vmax=vmax, extend='both')
             else:
-                im = ax.contourf(x, y, data_mean.T, levels=levels, cmap=cmap,
+                im = ax.contourf(x, y, data.T, levels=levels, cmap=cmap,
                                  vmin=vmin, vmax=vmax, extend='both')
         except TypeError as e:
             logger.warning('Could not evaluate levels: {}'.format(e))
             if invert_axis:
-                im = ax.contourf(x, y, data_mean, cmap=cmap,
+                im = ax.contourf(x, y, data, cmap=cmap,
                                  vmin=vmin, vmax=vmax, extend='both')
             else:
-                im = ax.contourf(x, y, data_mean.T, cmap=cmap,
+                im = ax.contourf(x, y, data.T, cmap=cmap,
                                  vmin=vmin, vmax=vmax, extend='both')
     else:  # pcolormesh
         if invert_axis:
-            im = ax.pcolormesh(x, y, data_mean, cmap=cmap,
+            im = ax.pcolormesh(x, y, data, cmap=cmap,
                                vmin=vmin, vmax=vmax, extend='both')
         else:
-            im = ax.pcolormesh(x, y, data_mean.T, cmap=cmap,
+            im = ax.pcolormesh(x, y, data.T, cmap=cmap,
                                vmin=vmin, vmax=vmax, extend='both')
 
     # Adjust the location of the subplots on the page to make room for the colorbar
@@ -143,23 +156,24 @@ def plot_hovmoller(data: xr.DataArray,
     cbar_ax = fig.add_axes([0.2, 0.15, 0.6, 0.02])
 
     # Add min and max values of the dim on the top right corner
-    ax.text(0.99, 0.99, f'{dim} = {dim_min:.2f} to {dim_max:.2f}',
-            verticalalignment='top', horizontalalignment='right',
-            transform=ax.transAxes, fontsize=12,
-            bbox=dict(facecolor='white', alpha=0.5))
+    if dim is not None:
+        ax.text(0.99, 0.99, label_text,
+                verticalalignment='top', horizontalalignment='right',
+                transform=ax.transAxes, fontsize=12,
+                bbox=dict(facecolor='white', alpha=0.5))
 
     # cbar label
     if cbar_label is None:
         try:
-            var_name = data_mean.short_name
+            var_name = data.short_name
         except AttributeError:
             try:
-                var_name = data_mean.long_name
+                var_name = data.long_name
             except AttributeError:
                 var_name = None
         # Add units
         try:
-            units = data_mean.units
+            units = data.units
         except AttributeError:
             units = None
         if var_name is not None and units is not None:
@@ -172,6 +186,18 @@ def plot_hovmoller(data: xr.DataArray,
 
     fig.colorbar(im, cax=cbar_ax, orientation='horizontal',
                 label=cbar_label)
+    
+    # Set log scale:
+    if kwargs.get('ylog', False):
+        ax.set_yscale('log')
+    if kwargs.get('xlog', False):
+        ax.set_xscale('log')
+
+    # Set axis inversion
+    if kwargs.get('xinvert', False):
+        ax.invert_xaxis()
+    if kwargs.get('yinvert', False):
+        ax.invert_yaxis()
 
     # Save the figure
     if save:
