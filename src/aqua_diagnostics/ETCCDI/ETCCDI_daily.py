bf41bbf86ec5032f09f12ad4d64480ca645af8cb
mnurisso <m.nurisso@isac.cnr.it>
2025-02-05 13:54:37 +0200

    attempt to sketch the class

--- src/aqua_diagnostics/ETCCDI/ETCCDI_daily.py
+++ src/aqua_diagnostics/ETCCDI/ETCCDI_daily.py
@@ -2,30 +2,18 @@ import aqua
 from aqua.logger import log_configure
 
 
-available_indices = ['fd', 'id', 'su', 'tr']
-
-
 class ETCCDI_daily ():
 
-    def __init__(self, Reader: aqua.Reader = None, index: str = None, year: int = None,
-                 longname: str = None, var: str = None,
-                 condition: str = None, condition_sign: str = None,
-                 statistic: str = None, units: str = 'days',
-                 cmap: str = 'Blues', loglevel: str = 'WARNING'):
+    def __init__(self, Reader: aqua.Reader = None, year: int = None,
+                 loglevel: str = 'WARNING'):
         """
         Initialize the ETCCDI_daily class.
+        This is a Mixin class for each individual ETCCDI index based on daily data.
 
         Arguments:
             Reader (aqua.Reader):  A Reader object. Defaults to None.
-            index (str):  The ETCCDI index to be calculated. Defaults to None.
             year (int, opt):  The year to be analyzed. Defaults to None.
-            longname (str, opt):  The long name of the ETCCDI index. Defaults to None.
-            var (str):  The variable to be analyzed. Defaults to None.
-            condition (str):  The condition to be applied. Defaults to None.
-            condition_sign (str):  The condition sign to be applied. Defaults to None.
-            statistic (str):  The statistic to be applied. Defaults to None.
-            units (str):  The units of the ETCCDI index. Defaults to 'days'.
-            cmap (str):  The colormap to be used. Defaults to 'Blues'.
+            loglevel (str, opt):  The logging level. Defaults to 'WARNING'.
         """
 
         if Reader is None or not isinstance(Reader, aqua.Reader):
@@ -33,48 +21,15 @@ class ETCCDI_daily ():
         else:
             self.Reader = Reader
 
-        if index is None or index not in available_indices:
-            raise ValueError(f"Index is not provided or not a valid ETCCDI index, available indices: {available_indices}")
-        else:
-            self.index = index
+        self.logger = log_configure(loglevel, 'ETCCDI_daily')
 
         if year is None:
             raise ValueError("Year is not provided.")
         else:
             self.year = year
-
-        if var is None:
-            raise ValueError("Variable is not provided.")
-        else:
-            self.var = var
-
-        if condition is None:
-            raise ValueError("Condition is not provided.")
-        else:
-            self.condition = condition
-
-        if condition_sign is None:
-            raise ValueError("Condition sign is not provided.")
-        else:
-            self.condition_sign = condition_sign
-
-        if statistic is None:
-            raise ValueError("Statistic is not provided.")
-        else:
-            self.statistic = statistic
+            self.logger.debug(f"Year: {self.year}")
 
         self.aggregation = 'D'
-        self.units = units
-        self.cmap = cmap
-
-        if longname is None:
-            self.longname = f"ETCCDI_{index}"
-        else:
-            self.longname = longname
-
-        logname = f"ETCCDI_{index}"
-        self.logger = log_configure(loglevel, logname)
-        self.logger.info(f"ETCCDI index: {index}, valid configuration")
 
         self.catalog = Reader.catalog
         self.model = Reader.model
@@ -87,3 +42,17 @@ class ETCCDI_daily ():
 
         # set the streaming
         self.Reader.stream(startdate=self.startdate, enddate=self.enddate, aggregation=self.aggregation)
+
+    # def monthy_loop(self):
+    #     """
+    #     Loop through each month of the year and calculate the ETCCDI index.
+    #     """
+
+    #     for month in range(1, 13):
+    #         self.month = month
+    #         self.startdate = f"{self.year}{month:02d}01"
+    #         self.enddate = f"{self.year}{month:02d}31"
+    #         self.logger.debug(f"Month: {self.month} Start Date: {self.startdate} End Date: {self.enddate}")
+
+    #         self.Reader.stream(startdate=self.startdate, enddate=self.enddate, aggregation=self.aggregation)
+    #         self.calculate_index()
