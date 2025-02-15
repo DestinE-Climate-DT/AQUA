1fef24002f0a896c1b880ca4de4df74aa7c8c8da
mnurisso <m.nurisso@isac.cnr.it>
2025-02-15 17:10:29 +0200

    adapt cli to core template

--- src/aqua_diagnostics/etccdi/cli_sdii.py
+++ src/aqua_diagnostics/etccdi/cli_sdii.py
@@ -8,6 +8,7 @@ from aqua import __version__ as aquaversion
 from aqua.logger import log_configure
 from aqua.util import get_arg, load_yaml
 from aqua.util import ConfigPath
+from aqua.diagnostics.core import template_parse_arguments, open_cluster, close_cluster
 from aqua.diagnostics.etccdi import SDII
 
 
@@ -16,50 +17,33 @@ def parse_arguments(cli_args):
 
     parser = argparse.ArgumentParser(description='ETCCDI SDII CLI')
 
-    parser.add_argument('-c', '--config', type=str,
-                        help='yaml configuration file')
+    parser = template_parse_arguments(parser)
+
     parser.add_argument('-n', '--nworkers', type=int,
                         help='number of dask distributed workers')
-    parser.add_argument('-l', '--loglevel', type=str,
-                        help='log level [default: WARNING]')
     parser.add_argument("--cluster", type=str,
                         required=False, help="dask cluster address")
-    parser.add_argument('--catalog', type=str, help='catalog name',
-                        required=False)
-    parser.add_argument('--model', type=str, help='model name',
-                        required=False)
-    parser.add_argument('--exp', type=str, help='experiment name',
-                        required=False)
-    parser.add_argument('--source', type=str, help='source name',
-                        required=False)
     parser.add_argument('--outputdir', type=str, help='output directory',
                         required=False)
     parser.add_argument('--year', type=int, help='year',
                         required=False)
+    parser.add_argument('--month', type=int, help='month to start the calculation from',
+                        required=False)
 
     return parser.parse_args(cli_args)
 
+
 if __name__ == "__main__":
-    
+
     args = parse_arguments(sys.argv[1:])
     loglevel = get_arg(args, 'loglevel', 'WARNING')
     logger = log_configure(log_name='ETCCDI-SDII', log_level=loglevel)
 
     logger.info(f'Running AQUA v{aquaversion} Teleconnections diagnostic')
-   # Dask distributed cluster
+
     nworkers = get_arg(args, 'nworkers', None)
     cluster = get_arg(args, 'cluster', None)
-    private_cluster = False
-    if nworkers or cluster:
-        if not cluster:
-            cluster = LocalCluster(n_workers=nworkers, threads_per_worker=1)
-            logger.info(f"Initializing private cluster {cluster.scheduler_address} with {nworkers} workers.")
-            private_cluster = True
-        else:
-            logger.info(f"Connecting to cluster {cluster}.")
-        client = Client(cluster)
-    else:
-        client = None
+    client, cluster, private_cluster = open_cluster(nworkers, cluster, loglevel)
 
     configdir = ConfigPath(loglevel=loglevel).configdir
     default_config = os.path.join(configdir, "diagnostics", "etccdi",
@@ -74,21 +58,16 @@ if __name__ == "__main__":
     exp = get_arg(args, 'exp', models[0]['exp'])
     source = get_arg(args, 'source', models[0]['source'])
     year = get_arg(args, 'year', config['year'])
+    month = get_arg(args, 'month', 1)
 
     sdii_flag = config['indices'].get('SDII', False)
 
     if sdii_flag:
         sdii = SDII(model=model, exp=exp, source=source, catalog=catalog,
-                    year=year, loglevel=loglevel)
+                    year=year, loglevel=loglevel, month=month)
         sdii.compute_index()
         sdii.combine_monthly_index()
 
-    if client:
-        client.close()
-        logger.debug("Dask client closed.")
+    close_cluster(client, cluster, private_cluster, loglevel)
 
-    if private_cluster:
-        cluster.close()
-        logger.debug("Dask cluster closed.")
-    
     logger.info('ETCCDI SDII diagnostic finished.')
