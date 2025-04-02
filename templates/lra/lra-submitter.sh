96ea586965415d4e7a534164f200379394b8a3d8
mnurisso <m.nurisso@isac.cnr.it>
2025-04-02 12:09:01 +0300

    setup for storylines LRA generation

---
+++ templates/lra/lra-submitter.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --partition=small
+#SBATCH --job-name=LRA_italy
+#SBATCH --output=lra_italy_%j.out
+#SBATCH --error=lra_italy_%j.err
+#SBATCH --account=project_465000454
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=24:00:00
+#SBATCH --mem=200G 
+set -e
+
+# set the number of dask workers
+# should be equal to the number of the total tasks available
+workers=1
+
+# run the Python script
+# -c to specify the configuration file
+# -f to use the fixer on data
+# -d to perform a dry run (to check generated lra)
+# -o to overwrite existing lra
+# -l to set the log level (default is WARNING)
+aqua lra --config lra_italy_prec.yaml -w 1 -d -l INFO
