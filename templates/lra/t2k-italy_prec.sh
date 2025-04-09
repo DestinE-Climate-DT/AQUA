e0428edf3fc4ad72a05ebfa604af36b95ed0b184
mnurisso <m.nurisso@isac.cnr.it>
2025-04-09 11:42:25 +0300

    parallelization of variables creation

--- templates/lra/t2k-italy_prec.sh
+++ templates/lra/t2k-italy_prec.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #SBATCH --partition=small
 #SBATCH --job-name=LRA_italy
-#SBATCH --output=lra_italy_%j.out
-#SBATCH --error=lra_italy_%j.err
+#SBATCH --output=t2k_italy_%j.out
+#SBATCH --error=t2k_italy_%j.err
 #SBATCH --account=project_465000454
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=4
 #SBATCH --time=24:00:00
 #SBATCH --mem=200G 
 set -e
 
 # set the number of dask workers
 # should be equal to the number of the total tasks available
-workers=1
+workers=4
 
 # run the Python script
 # -c to specify the configuration file
@@ -20,4 +20,4 @@ workers=1
 # -d to perform a dry run (to check generated lra)
 # -o to overwrite existing lra
 # -l to set the log level (default is WARNING)
-aqua lra --config lra_italy_prec.yaml -w 1 -d -l INFO
+aqua lra --config lra_italy_prec-t2k.yaml -w 4 -d -l INFO
