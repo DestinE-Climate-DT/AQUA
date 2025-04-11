d4e624b383bfe5ac24b50667aaea39b2db90d8e0
mnurisso <m.nurisso@isac.cnr.it>
2025-04-11 11:02:51 +0300

    refactor: update LRA templates to reduce variable complexity and adjust worker settings

--- templates/lra/historical-italy_prec.sh
+++ templates/lra/historical-italy_prec.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #SBATCH --partition=small
-#SBATCH --job-name=LRA_italy
+#SBATCH --job-name=2d-histo_italy
 #SBATCH --output=historical_italy_%j.out
 #SBATCH --error=historical_italy_%j.err
 #SBATCH --account=project_465000454
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=1
 #SBATCH --time=24:00:00
 #SBATCH --mem=200G 
 set -e
 
 # set the number of dask workers
 # should be equal to the number of the total tasks available
-workers=4
+workers=1
 
 # run the Python script
 # -c to specify the configuration file
@@ -20,4 +20,4 @@ workers=4
 # -d to perform a dry run (to check generated lra)
 # -o to overwrite existing lra
 # -l to set the log level (default is WARNING)
-aqua lra --config lra_italy_prec-historical.yaml -w 4 -d -l INFO
+aqua lra --config lra_italy_prec-historical.yaml -w 1 -d -l INFO
