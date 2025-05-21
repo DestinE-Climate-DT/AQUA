cd7f55e91265ebeaabe3a3dd9b8cb97517af788d
silviacaprioli <silvietta.caprioli@gmail.com>
2025-05-21 17:44:56 +0300

    add regrid r100 in setup_checker

--- src/aqua_diagnostics/cli/cli_checker.py
+++ src/aqua_diagnostics/cli/cli_checker.py
@@ -61,6 +61,7 @@ if __name__ == '__main__':
     model = get_arg(args, 'model', None)
     exp = get_arg(args, 'exp', None)
     source = get_arg(args, 'source', None)
+    regrid = get_arg(args, 'regrid', None)
     yamldir = get_arg(args, 'yaml', None)
     fread = args.no_read
     frebuild = args.no_rebuild
@@ -72,7 +73,7 @@ if __name__ == '__main__':
 
     try:
         reader = Reader(catalog=catalog, model=model, exp=exp, source=source,
-                        loglevel=loglevel, rebuild=frebuild)
+                        regrid=regrid, loglevel=loglevel, rebuild=frebuild)
 
         # extract metadata from catalog
         if yamldir:
