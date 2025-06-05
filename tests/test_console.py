aaa3e06ae67ae9534cf888344e1e07f1fb2881f1
Paolo Davini <p.davini@isac.cnr.it>
2025-06-05 21:33:50 +0200

    console test fix

--- tests/test_console.py
+++ tests/test_console.py
@@ -261,7 +261,7 @@ class TestAquaConsole():
         # run the LRA and verify that at least one file exist
         run_aqua(['lra', '--config', lratest, '-w', '1', '-d', '--rebuild'])
         path = os.path.join(os.path.join(mydir, 'lra_test'),
-                            "ci/IFS/test-tco79/r200/monthly/2t_test-tco79_r200_monthly_mean_202002.nc")
+                            "ci/IFS/test-tco79/r1/r200/monthly/mean/2t_test-tco79_r1_r200_monthly_mean_global_202002.nc")
         assert os.path.isfile(path)
 
         # run the LRA with a different stat and verify that at least one file exist
