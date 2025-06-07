cb3c5045bb671b7352567056cf56d28fd379edd8
Matteo Nurisso <m.nurisso@isac.cnr.it>
2025-06-07 13:44:37 +0300

    QBO and PlotQBO in init file

--- src/aqua_diagnostics/teleconnections/__init__.py
+++ src/aqua_diagnostics/teleconnections/__init__.py
@@ -2,11 +2,13 @@
 from .enso import ENSO
 from .mjo import MJO, PlotMJO
 from .nao import NAO
+from .qbo import QBO, PlotQBO
 from .plot_enso import PlotENSO
 from .plot_nao import PlotNAO
 
 __all__ = ['ENSO',
            'MJO', 'PlotMJO',
            'NAO',
+           'QBO', 'PlotQBO',
            'PlotENSO',
            'PlotNAO']
