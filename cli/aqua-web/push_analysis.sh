46ea5ad8c582aa848022140f424af3fb0408bc15
Jost von Hardenberg <hardenberg@gmail.com>
2025-07-11 09:19:42 +0200

    first version

--- cli/aqua-web/push_analysis.sh
+++ cli/aqua-web/push_analysis.sh
@@ -118,8 +118,13 @@ convert_pdf_to_png() {
 
         mkdir -p $dstdir
     
-        IFS='/' read -r catalog model experiment <<< "$1"
-        $SCRIPT_DIR/pdf_to_png.sh "$catalog" "$model" "$experiment"
+        if [ $ensemble -eq 1 ]; then
+            IFS='/' read -r catalog model experiment realization <<< "$1"
+            $SCRIPT_DIR/pdf_to_png.sh "$catalog" "$model" "$experiment" "$realization"
+        else
+            IFS='/' read -r catalog model experiment <<< "$1"
+            $SCRIPT_DIR/pdf_to_png.sh "$catalog" "$model" "$experiment"
+        fi
     fi
 }
 
@@ -134,6 +139,7 @@ print_help() {
     echo "  -b, --bucket BUCKET    push to the specified bucket (defaults to 'aqua-web')"
     echo "  -c, --config FILE      alternate config file to determine diagnostic groupings for make_contents (defaults to config.grouping.yaml)"
     echo "  -d, --no-update        do not update the remote github repository"  
+    echo "  -e, --ensemble         use new ensemble structure"
     echo "  -h, --help             display this help and exit"
     echo "  -l, --loglevel LEVEL   set the log level (1=DEBUG, 2=INFO, 3=WARNING, 4=ERROR, 5=CRITICAL). Default is 2."
     echo "  -n, --no-convert       do not convert PDFs to PNGs (use only if all PNGs are already available)"  
@@ -158,6 +164,7 @@ repository="DestinE-Climate-DT/aqua-web"
 update=1
 rsync=""
 config="$SCRIPT_DIR/config.grouping.yaml"
+ensemble=0
 
 while [[ $# -gt 2 ]]; do
   case "$1" in
@@ -172,6 +179,10 @@ while [[ $# -gt 2 ]]; do
     -l|--loglevel)
         loglevel="$2"
         shift 2
+        ;;
+    -e|--ensemble)
+        ensemble=1
+        shift
         ;;
     -d|--no-update)
         update=0
@@ -273,14 +284,23 @@ if [ -f "$exps" ]; then
         catalog=$(echo "$line" | awk '{print $1}')
         model=$(echo "$line" | awk '{print $2}')
         experiment=$(echo "$line" | awk '{print $3}')
+        if [ $ensemble -eq 1 ]; then
+            realization=$(echo "$line" | awk '{print $4}')
+            realization=${realization:-r1}  # Default to r1 if not specified
+        fi
 
-        log_message INFO "Collect figures for $catalog/$model/$experiment and converting to png"
-        collect_figures "$1" "$catalog/$model/$experiment"
-        convert_pdf_to_png "$catalog/$model/$experiment"
+        if [ $ensemble -eq 1 ]; then
+            expstr="$catalog/$model/$experiment/$realization"
+        else
+            expstr="$catalog/$model/$experiment"
+        fi
+        log_message INFO "Collect figures for $expstr and converting to png"
+        collect_figures "$1" "$expstr"
+        convert_pdf_to_png "$expstr"
         get_file $bucket content/experiments.yaml content/experiments.yaml "$rsync"  # recover experiments.yaml file
-        make_contents "$catalog/$model/$experiment" "$config" # create catalog.yaml and catalog.json and update experiments.yaml
-        push_lumio $bucket "$catalog/$model/$experiment" "$rsync"  # push figures including experiments.yaml
-        echo "$catalog/$model/$experiment" >> updated.txt
+        make_contents "$expstr" "$config" # create catalog.yaml and catalog.json and update experiments.yaml
+        push_lumio $bucket "$expstr" "$rsync"  # push figures including experiments.yaml
+        echo "$expstr" >> updated.txt
     done < "$exps"
 else  # Otherwise, use the second argument as the experiment folder
     log_message INFO "Collect figures for $exps and converting to png"
