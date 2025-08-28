99c6525c6c964c836816b4e57b5543ffb3e34f1e
ghosh97 <supriyo.ghosh@bsc.es>
2025-08-28 13:38:32 +0200

    Adjust super title font size in plot_maps based on number of rows for better visibility

--- src/aqua_diagnostics/ocean_trends/multiple_map.py
+++ src/aqua_diagnostics/ocean_trends/multiple_map.py
@@ -189,7 +189,7 @@ def plot_maps(
     # Add a super title
     if title:
         logger.debug("Setting super title to %s", title)
-        fig.suptitle(title, fontsize=16)
+        fig.suptitle(title, fontsize=nrows * 10)
 
     if return_fig:
         return fig
