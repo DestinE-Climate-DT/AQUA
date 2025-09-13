051bc8e4c9ffb0f055783c7058fe775f6db0d175
mnurisso <m.nurisso@isac.cnr.it>
2025-09-13 22:41:27 +0200

    refactor diagnostics

--- src/aqua_diagnostics/seaice/seaice.py
+++ src/aqua_diagnostics/seaice/seaice.py
@@ -4,7 +4,7 @@ import xarray as xr
 from aqua.diagnostics.core import Diagnostic
 from aqua.exceptions import NoDataError
 from aqua.logger import log_configure, log_history
-from aqua.util import area_selection, to_list, merge_attrs
+from aqua.util import to_list, merge_attrs
 from aqua.diagnostics.seaice.util import ensure_istype
 
 xr.set_options(keep_attrs=True)
