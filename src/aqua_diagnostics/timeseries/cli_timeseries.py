74fd40ae7478785bde5e02e16a13abee3c0c9027
mnurisso <m.nurisso@isac.cnr.it>
2025-09-18 10:36:50 +0200

    updated reader_kwargs logic

--- src/aqua_diagnostics/timeseries/cli_timeseries.py
+++ src/aqua_diagnostics/timeseries/cli_timeseries.py
@@ -54,11 +54,11 @@ if __name__ == '__main__':
     regrid = get_arg(args, 'regrid', None)
     logger.info(f"Regrid option is set to {regrid}")
     realization = get_arg(args, 'realization', None)
-    # This reader_kwargs will be used if the dataset corresponding value is None
+    # This reader_kwargs will be used if the dataset corresponding value is None or not present
     if realization:
         reader_kwargs = {'realization': realization}
     else:
-        reader_kwargs = config_dict['datasets'][0].get('reader_kwargs', {})
+        reader_kwargs = config_dict['datasets'][0]['reader_kwargs'] or {}
 
     # Output options
     outputdir = config_dict['output'].get('outputdir', './')
@@ -99,7 +99,7 @@ if __name__ == '__main__':
                             logger.debug(f"Dataset args: {dataset_args}")
                             ts[i] = Timeseries(**init_args, **dataset_args)
                             ts[i].run(**run_args, create_catalog_entry=create_catalog_entry,
-                                      reader_kwargs=dataset.get('reader_kwargs', reader_kwargs))
+                                      reader_kwargs=dataset['reader_kwargs'] or reader_kwargs)
 
                         # Reference datasets are evaluated on the maximum time range of the datasets
                         startdate = min([ts[i].startdate for i in range(len(ts))])
@@ -122,7 +122,7 @@ if __name__ == '__main__':
                                 logger.info(f"Reference args: {reference_args}")
                                 ts_ref[i] = Timeseries(**init_args, **reference_args)
                                 ts_ref[i].run(**run_args, std=True, create_catalog_entry=False,
-                                              reader_kwargs=reference.get('reader_kwargs', {}))
+                                              reader_kwargs=reference['reader_kwargs'] or {})
 
                         # Plot the timeseries
                         if save_pdf or save_png:
@@ -177,7 +177,7 @@ if __name__ == '__main__':
                                             'regrid': regrid if regrid is not None else dataset.get('regrid', None)}
                             ts[i] = Timeseries(**init_args, **dataset_args)
                             ts[i].run(**run_args, create_catalog_entry=create_catalog_entry,
-                                      reader_kwargs=dataset.get('reader_kwargs', reader_kwargs))
+                                      reader_kwargs=dataset['reader_kwargs'] or reader_kwargs)
 
                         # Reference datasets are evaluated on the maximum time range of the datasets
                         startdate = min([ts[i].plt_startdate for i in range(len(ts))])
@@ -196,7 +196,7 @@ if __name__ == '__main__':
                                                 'regrid': regrid if regrid is not None else reference.get('regrid', None)}
                                 ts_ref[i] = Timeseries(**init_args, **reference_args)
                                 ts_ref[i].run(**run_args, std=True, create_catalog_entry=False,
-                                              reader_kwargs=reference.get('reader_kwargs', {}))
+                                              reader_kwargs=reference['reader_kwargs'] or {})
 
                         # Plot the timeseries
                         if save_pdf or save_png:
@@ -244,8 +244,7 @@ if __name__ == '__main__':
                         init_args = {'region': region, 'loglevel': loglevel, 'diagnostic_name': diagnostic_name}
                         run_args = {'var': var, 'formula': False, 'long_name': var_config.get('long_name'),
                                     'units': var_config.get('units'), 'short_name': var_config.get('short_name'),
-                                    'outputdir': outputdir, 'rebuild': rebuild, 'center_time': center_time,
-                                    'reader_kwargs': reader_kwargs}
+                                    'outputdir': outputdir, 'rebuild': rebuild, 'center_time': center_time}
 
                         # Initialize a list of len from the number of datasets
                         sc = [None] * len(config_dict['datasets'])
@@ -257,7 +256,7 @@ if __name__ == '__main__':
                                             'regrid': regrid if regrid is not None else dataset.get('regrid', None)}
                             sc[i] = SeasonalCycles(**init_args, **dataset_args)
                             sc[i].run(**run_args, create_catalog_entry=create_catalog_entry,
-                                      reader_kwargs=dataset.get('reader_kwargs', reader_kwargs))
+                                      reader_kwargs=dataset['reader_kwargs'] or reader_kwargs)
 
                         # Reference datasets are evaluated on the maximum time range of the datasets
                         startdate = min([sc[i].startdate for i in range(len(sc))])
@@ -276,7 +275,7 @@ if __name__ == '__main__':
                                                 'regrid': regrid if regrid is not None else reference.get('regrid', None)}
                                 sc_ref[i] = SeasonalCycles(**init_args, **reference_args)
                                 sc_ref[i].run(**run_args, std=True, create_catalog_entry=False,
-                                              reader_kwargs=reference.get('reader_kwargs', {}))
+                                              reader_kwargs=reference['reader_kwargs'] or {})
 
                         # Plot the seasonal cycles
                         if save_pdf or save_png:
@@ -329,7 +328,7 @@ if __name__ == '__main__':
                                     'regrid': regrid if regrid is not None else dataset.get('regrid', None)}
 
                     greg[i] = Gregory(**init_args, **dataset_args)
-                    greg[i].run(**run_args, **model_args, reader_kwargs=dataset.get('reader_kwargs', {}))
+                    greg[i].run(**run_args, **model_args, reader_kwargs=dataset['reader_kwargs'] or reader_kwargs)
 
                 if config_dict['diagnostics']['gregory']['std']:
                     # t2m:
