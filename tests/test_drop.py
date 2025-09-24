6fef6632f97b9808fed706c23bd891bea44cc084
Marco Cadau <marco.cadau@polito.it>
2025-09-24 10:35:24 +0300

    `lra_arguments` variable rename fix in `test_drop`

--- tests/test_drop.py
+++ tests/test_drop.py
@@ -13,7 +13,7 @@ LRAPATH_DAILY = 'ci/IFS/test-tco79/r1/r100/daily/mean/europe'
 
 
 @pytest.fixture(params=[{"model": "IFS", "exp": "test-tco79", "source": "long", "var": "2t", "outdir": "lra_test"}])
-def lra_arguments(request):
+def drop_arguments(request):
     """Provides DROP arguments as a dictionary."""
     return request.param
 
@@ -33,22 +33,22 @@ class TestOutputPathBuilder:
         (None, None, None, 'europe', None, expected[1]),
         ('r200', 'daily', 'r2', 'global', 'nostat', expected[2]),
     ])
-    def test_build_path(self, lra_arguments, resolution, frequency, realization, region, stat, expected):
+    def test_build_path(self, drop_arguments, resolution, frequency, realization, region, stat, expected):
         """Test building output path."""
         builder = OutputPathBuilder(
-            catalog='ci', model=lra_arguments["model"], exp=lra_arguments["exp"],
+            catalog='ci', model=drop_arguments["model"], exp=drop_arguments["exp"],
             resolution=resolution,
             frequency=frequency, realization=realization, stat=stat, region=region)
         path = builder.build_path(
-            os.path.join(os.getcwd(), lra_arguments['outdir']),
-            var=lra_arguments['var'], year=2020, month=1)
+            os.path.join(os.getcwd(), drop_arguments['outdir']),
+            var=drop_arguments['var'], year=2020, month=1)
 
         if not expected:
             lrapath = f'ci/IFS/test-tco79/{realization}/{resolution}/{frequency}/{stat}/{region}'
-            expected = os.path.join(os.getcwd(), lra_arguments["outdir"], lrapath,
+            expected = os.path.join(os.getcwd(), drop_arguments["outdir"], lrapath,
                                          f"2t_ci_IFS_test-tco79_{realization}_{resolution}_{frequency}_{stat}_{region}_202001.nc")
         else:
-            expected = os.path.join(os.getcwd(), lra_arguments["outdir"], expected)
+            expected = os.path.join(os.getcwd(), drop_arguments["outdir"], expected)
 
         assert path == expected
 
@@ -60,25 +60,25 @@ class TestCatalogEntryBuilder:
         ('r100', 'monthly', 'r1', 'global', 'mean'),
         ('r200', 'daily', 'r1', 'global', 'mean'),
       ])
-    def test_create_entry_name(self, lra_arguments, resolution, frequency, realization, region, stat):
+    def test_create_entry_name(self, drop_arguments, resolution, frequency, realization, region, stat):
         """Test creation of entry name."""
         builder = CatalogEntryBuilder(
-            catalog='ci', **lra_arguments,
+            catalog='ci', **drop_arguments,
             resolution=resolution, frequency=frequency, realization=realization,
             stat=stat, region=region, loglevel=LOGLEVEL
         )
         entry_name = builder.create_entry_name()
-        block = builder.create_entry_details(basedir=lra_arguments["outdir"], source_grid_name='lon-lat')
+        block = builder.create_entry_details(basedir=drop_arguments["outdir"], source_grid_name='lon-lat')
         assert entry_name == f'lra-{resolution}-{frequency}'
         assert block['driver'] == 'netcdf'
         assert block['parameters'].keys() == {'realization', 'stat', 'region'}
 
         builder2 = CatalogEntryBuilder(
-            catalog='ci', **lra_arguments,
+            catalog='ci', **drop_arguments,
             resolution=resolution, frequency=frequency, realization='r2',
             stat=stat, region=region, loglevel=LOGLEVEL
         )
-        newblock = builder2.create_entry_details(basedir=lra_arguments["outdir"], catblock=block, source_grid_name='lon-lat')
+        newblock = builder2.create_entry_details(basedir=drop_arguments["outdir"], catblock=block, source_grid_name='lon-lat')
         assert newblock['args']['urlpath'] == block['args']['urlpath']
         assert newblock['parameters']['realization']['allowed'] == ['r1','r2']
 
@@ -88,22 +88,22 @@ class TestCatalogEntryBuilder:
 class TestLRA:
     """Class containing LRA tests."""
 
-    def test_definitive_false(self, lra_arguments, tmp_path):
+    def test_definitive_false(self, drop_arguments, tmp_path):
         """Test DROP with definitive=False."""
         test = Drop(
-            catalog='ci', **lra_arguments, tmpdir=str(tmp_path),
+            catalog='ci', **drop_arguments, tmpdir=str(tmp_path),
             resolution='r100', frequency='monthly', loglevel=LOGLEVEL
         )
 
         test.retrieve()
         test.generate_lra()
-        assert os.path.isdir(os.path.join(os.getcwd(), lra_arguments["outdir"], LRAPATH))
-        shutil.rmtree(os.path.join(lra_arguments["outdir"]))
+        assert os.path.isdir(os.path.join(os.getcwd(), drop_arguments["outdir"], LRAPATH))
+        shutil.rmtree(os.path.join(drop_arguments["outdir"]))
 
     @pytest.mark.parametrize("nworkers", [1, 2])
-    def test_definitive_true(self, lra_arguments, tmp_path, nworkers):
+    def test_definitive_true(self, drop_arguments, tmp_path, nworkers):
         test = Drop(
-            catalog='ci', **lra_arguments, tmpdir=str(tmp_path),
+            catalog='ci', **drop_arguments, tmpdir=str(tmp_path),
             nproc=nworkers, resolution='r100', frequency='monthly', 
             definitive=True, loglevel=LOGLEVEL
         )
@@ -112,21 +112,21 @@ class TestLRA:
         test.data = test.data.sel(time="2020-01")
         test.generate_lra()
 
-        file_path = os.path.join(os.getcwd(), lra_arguments["outdir"], LRAPATH, "2t_ci_IFS_test-tco79_r1_r100_monthly_mean_global_202001.nc")
-        test.check_integrity(varname=lra_arguments["var"])
+        file_path = os.path.join(os.getcwd(), drop_arguments["outdir"], LRAPATH, "2t_ci_IFS_test-tco79_r1_r100_monthly_mean_global_202001.nc")
+        test.check_integrity(varname=drop_arguments["var"])
         assert os.path.isfile(file_path)
 
         file = xr.open_dataset(file_path)
         assert len(file.time) == 1
         assert pytest.approx(file['2t'][0, 1, 1].item()) == 248.0704
-        shutil.rmtree(os.path.join(lra_arguments["outdir"]))
+        shutil.rmtree(os.path.join(drop_arguments["outdir"]))
 
-    def test_regional_subset(self, lra_arguments, tmp_path):
+    def test_regional_subset(self, drop_arguments, tmp_path):
         """Test DROP with regional subset."""
         region = {'name': 'europe', 'lon': [-10, 30], 'lat': [35, 70]}
 
         test = Drop(
-            catalog='ci', **lra_arguments, tmpdir=str(tmp_path),
+            catalog='ci', **drop_arguments, tmpdir=str(tmp_path),
             resolution='r100', frequency='daily', definitive=True,
             loglevel=LOGLEVEL, region=region
         )
@@ -135,18 +135,18 @@ class TestLRA:
         test.data = test.data.sel(time="2020-01-20")
         test.generate_lra()
 
-        file_path = os.path.join(os.getcwd(), lra_arguments["outdir"], LRAPATH_DAILY, "2t_ci_IFS_test-tco79_r1_r100_daily_mean_europe_202001.nc")
+        file_path = os.path.join(os.getcwd(), drop_arguments["outdir"], LRAPATH_DAILY, "2t_ci_IFS_test-tco79_r1_r100_daily_mean_europe_202001.nc")
         assert os.path.isfile(file_path), "File not found: {}".format(file_path)
 
         xfield = xr.open_dataset(file_path).where(lambda x: x.notnull(), drop=True)
         assert xfield.lat.min() > 35
         assert xfield.lat.max() < 70
-        shutil.rmtree(os.path.join(lra_arguments["outdir"]))
+        shutil.rmtree(os.path.join(drop_arguments["outdir"]))
 
-    def test_zarr_entry(self, lra_arguments, tmp_path):
+    def test_zarr_entry(self, drop_arguments, tmp_path):
         """Test DROP with Zarr archive creation."""
         test = Drop(
-            catalog='ci', **lra_arguments, tmpdir=str(tmp_path),
+            catalog='ci', **drop_arguments, tmpdir=str(tmp_path),
             resolution='r100', frequency='monthly', nproc=1,
             loglevel=LOGLEVEL, definitive=True
         )
@@ -156,31 +156,31 @@ class TestLRA:
         test.create_catalog_entry()
         test.create_zarr_entry()
 
-        reader1 = Reader(model=lra_arguments["model"], exp=lra_arguments["exp"], source='lra-r100-monthly')
-        reader2 = Reader(model=lra_arguments["model"], exp=lra_arguments["exp"], source='lra-r100-monthly-zarr')
+        reader1 = Reader(model=drop_arguments["model"], exp=drop_arguments["exp"], source='lra-r100-monthly')
+        reader2 = Reader(model=drop_arguments["model"], exp=drop_arguments["exp"], source='lra-r100-monthly-zarr')
 
         data1 = reader1.retrieve()
         data2 = reader2.retrieve()
         assert data1.equals(data2)
-        shutil.rmtree(os.path.join(lra_arguments["outdir"]))
+        shutil.rmtree(os.path.join(drop_arguments["outdir"]))
 
-    def test_dask_overwrite(self, lra_arguments, tmp_path):
+    def test_dask_overwrite(self, drop_arguments, tmp_path):
         """Test DROP with overwrite=True and Dask initialization."""
         test = Drop(
-            catalog='ci', **lra_arguments, tmpdir=str(tmp_path),
+            catalog='ci', **drop_arguments, tmpdir=str(tmp_path),
             resolution='r100', frequency='monthly', nproc=4,
             loglevel=LOGLEVEL, definitive=True, overwrite=True
         )
 
         test.retrieve()
         test.generate_lra()
-        assert os.path.isdir(os.path.join(os.getcwd(), lra_arguments["outdir"], LRAPATH))
-        shutil.rmtree(os.path.join(lra_arguments["outdir"]))
+        assert os.path.isdir(os.path.join(os.getcwd(), drop_arguments["outdir"], LRAPATH))
+        shutil.rmtree(os.path.join(drop_arguments["outdir"]))
 
-    def test_exclude_incomplete(self, lra_arguments, tmp_path):
+    def test_exclude_incomplete(self, drop_arguments, tmp_path):
         """Test DROP's exclude_incomplete option."""
         test = Drop(
-            catalog='ci', **lra_arguments, tmpdir=str(tmp_path),
+            catalog='ci', **drop_arguments, tmpdir=str(tmp_path),
             resolution='r100', frequency='monthly', definitive=True,
             loglevel=LOGLEVEL, exclude_incomplete=True
         )
@@ -188,61 +188,61 @@ class TestLRA:
         test.retrieve()
         test.generate_lra()
 
-        missing_file = os.path.join(os.getcwd(), lra_arguments["outdir"], LRAPATH, "2t_ci_IFS_test-tco79_r1_r100_monthly_mean_global_202008.nc")
-        existing_file = os.path.join(os.getcwd(), lra_arguments["outdir"], LRAPATH, "2t_ci_IFS_test-tco79_r1_r100_monthly_mean_global_202002.nc")
+        missing_file = os.path.join(os.getcwd(), drop_arguments["outdir"], LRAPATH, "2t_ci_IFS_test-tco79_r1_r100_monthly_mean_global_202008.nc")
+        existing_file = os.path.join(os.getcwd(), drop_arguments["outdir"], LRAPATH, "2t_ci_IFS_test-tco79_r1_r100_monthly_mean_global_202002.nc")
 
         assert not os.path.exists(missing_file)
         assert os.path.exists(existing_file)
 
         file = xr.open_dataset(existing_file)
         assert len(file.time) == 1
-        test.check_integrity(varname=lra_arguments["var"])
-        shutil.rmtree(os.path.join(lra_arguments["outdir"]))
+        test.check_integrity(varname=drop_arguments["var"])
+        shutil.rmtree(os.path.join(drop_arguments["outdir"]))
 
-    def test_concat_var_year(self, lra_arguments, tmp_path):
+    def test_concat_var_year(self, drop_arguments, tmp_path):
         """Test concatenation of monthly files into a single yearly file."""
         resolution = 'r100'
         frequency = 'monthly'
         year = 2022
 
         test = Drop(
-            catalog='ci', **lra_arguments, tmpdir=str(tmp_path),
+            catalog='ci', **drop_arguments, tmpdir=str(tmp_path),
             resolution=resolution, frequency=frequency, loglevel=LOGLEVEL
         )
 
         for month in range(1, 13):
             mm = f'{month:02d}'
-            filename = test.get_filename(lra_arguments["var"], year, month=mm)
+            filename = test.get_filename(drop_arguments["var"], year, month=mm)
             timeobj = pd.Timestamp(f'{year}-{mm}-01')
-            ds = xr.Dataset({lra_arguments["var"]: xr.DataArray([0], dims=['time'], coords={'time': [timeobj]})})
+            ds = xr.Dataset({drop_arguments["var"]: xr.DataArray([0], dims=['time'], coords={'time': [timeobj]})})
             ds.to_netcdf(filename)
 
-        test._concat_var_year(lra_arguments["var"], year)
-        outfile = test.get_filename(lra_arguments["var"], year)
+        test._concat_var_year(drop_arguments["var"], year)
+        outfile = test.get_filename(drop_arguments["var"], year)
 
         assert os.path.exists(outfile)
-        shutil.rmtree(os.path.join(lra_arguments["outdir"]))
+        shutil.rmtree(os.path.join(drop_arguments["outdir"]))
 
-    def test_concat_var_year_cdo(self, lra_arguments, tmp_path):
+    def test_concat_var_year_cdo(self, drop_arguments, tmp_path):
         """Test concatenation of monthly files into a single yearly file using cdo."""
         resolution = 'r100'
         frequency = 'monthly'
         year = 2022
 
         test = Drop(
-            catalog='ci', **lra_arguments, tmpdir=str(tmp_path), compact="cdo",
+            catalog='ci', **drop_arguments, tmpdir=str(tmp_path), compact="cdo",
             resolution=resolution, frequency=frequency, loglevel=LOGLEVEL
         )
 
         for month in range(1, 13):
             mm = f'{month:02d}'
-            filename = test.get_filename(lra_arguments["var"], year, month=mm)
+            filename = test.get_filename(drop_arguments["var"], year, month=mm)
             timeobj = pd.Timestamp(f'{year}-{mm}-01')
-            ds = xr.Dataset({lra_arguments["var"]: xr.DataArray([0], dims=['time'], coords={'time': [timeobj]})})
+            ds = xr.Dataset({drop_arguments["var"]: xr.DataArray([0], dims=['time'], coords={'time': [timeobj]})})
             ds.to_netcdf(filename)
 
-        test._concat_var_year(lra_arguments["var"], year)
-        outfile = test.get_filename(lra_arguments["var"], year)
+        test._concat_var_year(drop_arguments["var"], year)
+        outfile = test.get_filename(drop_arguments["var"], year)
 
         assert os.path.exists(outfile)
-        shutil.rmtree(os.path.join(lra_arguments["outdir"]))
+        shutil.rmtree(os.path.join(drop_arguments["outdir"]))
