c43855d505ba1aa184285673b0d9c3c79bb80224
silviacaprioli <silvietta.caprioli@gmail.com>
2025-10-16 14:01:27 +0300

    add expid in catgen config

--- src/aqua/cli/catgen.py
+++ src/aqua/cli/catgen.py
@@ -328,7 +328,7 @@ class AquaFDBGenerator:
                 'author': self.author,
                 'maintainer': self.config.get('maintainer') or 'not specified',
                 'machine': self.machine,
-                'expid': self.config['expver'],
+                'expid': self.expid,
                 'resolution_atm': self.atm_grid,
                 'resolution_oce': self.ocean_grid,
                 'forcing': forcing,
