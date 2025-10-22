9d92392b2e0ee454ff225c0c7a864b580e154e25
oloapinivad <p.davini@isac.cnr.it>
2025-10-22 09:42:42 +0200

    boxplot clean

--- src/aqua_diagnostics/boxplots/cli_boxplots.py
+++ src/aqua_diagnostics/boxplots/cli_boxplots.py
@@ -6,52 +6,51 @@ from aqua.diagnostics.core import template_parse_arguments
 from aqua.diagnostics import Boxplots, PlotBoxplots
 from aqua_diagnostics.core import DiagnosticCLI
 
-DIAGNAME='boxplots'
+TOOLNAME='boxplots'
 
-def parse_arguments(args):
-    """Parse command-line arguments for GlobalBiases diagnostic.
+def parse_arguments(arguments):
+    """Parse command-line arguments for Boxplots diagnostic.
 
     Args:
         args (list): list of command-line arguments to parse.
     """
     parser = argparse.ArgumentParser(description='Boxplots CLI')
     parser = template_parse_arguments(parser)
-    return parser.parse_args(args)
+    return parser.parse_args(arguments)
 
 if __name__ == '__main__':
 
     args = parse_arguments(sys.argv[1:])
-    
+
     # Initialize CLI handler
     cli = DiagnosticCLI(
         args,
-        diagnostic_name=DIAGNAME,
+        diagnostic_name=TOOLNAME,
         config='config_radiation-boxplots.yaml',
-        log_name=f'{DIAGNAME} CLI'
+        log_name=f'{TOOLNAME} CLI'
     )
-    
+
     # Prepare CLI (load config, setup logging, etc.)
     cli.prepare()
 
     # Open Dask cluster if needed
     cli.open_dask_cluster()
 
-
     # Boxplots diagnostic
-    if DIAGNAME in cli.config_dict['diagnostics']:
-        if cli.config_dict['diagnostics'][DIAGNAME]['run']:
+    if TOOLNAME in cli.config_dict['diagnostics']:
+        if cli.config_dict['diagnostics'][TOOLNAME]['run']:
             cli.logger.info("Boxplots diagnostic is enabled.")
 
-            diagnostic_name = cli.config_dict['diagnostics'][DIAGNAME].get('diagnostic_name', DIAGNAME)
+            diagnostic_name = cli.config_dict['diagnostics'][TOOLNAME].get('diagnostic_name', TOOLNAME)
             datasets = cli.config_dict['datasets']
             references = cli.config_dict['references']
-            variable_groups = cli.config_dict['diagnostics'][DIAGNAME].get('variables', [])
+            variable_groups = cli.config_dict['diagnostics'][TOOLNAME].get('variables', [])
 
             for group in variable_groups:
                 variables = group.get('vars', [])
                 plot_kwargs = {k: v for k, v in group.items() if k != 'vars'}
 
-                cli.logger.info("Running %s for %s with options %s", DIAGNAME, variables, plot_kwargs)
+                cli.logger.info("Running %s for %s with options %s", TOOLNAME, variables, plot_kwargs)
 
                 fldmeans = []
                 for dataset in datasets:
@@ -60,7 +59,7 @@ if __name__ == '__main__':
                     boxplots = Boxplots(**dataset_args, save_netcdf=cli.save_netcdf, outputdir=cli.outputdir, loglevel=cli.loglevel)
                     boxplots.run(var=variables, reader_kwargs=cli.reader_kwargs)
                     fldmeans.append(boxplots.fldmeans)
-                
+
                 fldmeans_ref = []
                 for reference in references:
                     reference_args = cli.dataset_args(reference)
@@ -70,8 +69,11 @@ if __name__ == '__main__':
 
                     if getattr(boxplots_ref, "fldmeans", None) is None:
                         cli.logger.warning(
-                            f"No data retrieved for reference {reference['model']} ({reference['exp']}, {reference['source']}). Skipping."
-                     )
+                            "No data retrieved for reference %s (%s, %s). Skipping.",
+                            reference['model'],
+                            reference['exp'],
+                            reference['source']
+                        )
                         continue
 
                     fldmeans_ref.append(boxplots_ref.fldmeans)
