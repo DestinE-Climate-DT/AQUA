1f24333aea7358790748199d74c00cfe479672b1
Paolo Davini <p.davini@isac.cnr.it>
2025-11-21 14:16:29 +0100

    tentative

--- aqua/core/__init__.py
+++ aqua/core/__init__.py
@@ -1,5 +1,5 @@
 """AQUA Core module"""
-from aqua.core.src import *
-from aqua.core.src import __version__, __all__
+from .src import *
+from .src import __version__, __all__
 
 __all__ = __all__  # Re-export the __all__ from src
