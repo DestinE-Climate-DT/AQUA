c36b8bbb1a662e19a85c7219df1945cab704366d
Emanuele Tovazzi <tovaz92@gmail.com>
2025-12-01 13:04:35 +0100

    move files to clean

--- tests/utils_tests/cleanup.py
+++ tests/utils_tests/cleanup.py
@@ -7,6 +7,14 @@ preventing race conditions in parallel test execution.
 import os
 from aqua.core.logger import log_configure
 
+# Files created by tests that need cleanup. Paths are relative to configdir (e.g. ~/.aqua/)
+FILES_TO_CLEAN = [
+    # Grid files
+    'grids/regular.yaml',
+    'grids/nemo-curvilinear.yaml',
+    'grids/ifs-unstructured.yaml'
+    # Add other files here
+    ]
 
 class TestCleanupRegistry:
     """
@@ -15,15 +23,7 @@ class TestCleanupRegistry:
     This class provides a simple way to clean up test artifacts
     that are created in the AQUA configuration directory (~/.aqua).
     """
-    
-    # Grid files created by tests that need cleanup
-    # Paths are relative to configdir (e.g., ~/.aqua/grids/regular.yaml)
-    FILES_TO_CLEAN = [
-        'grids/regular.yaml',
-        'grids/nemo-curvilinear.yaml',
-        'grids/ifs-unstructured.yaml'
-    ]
-    
+        
     def __init__(self, configdir: str, loglevel: str = 'WARNING'):
         """
         Initialize the cleanup registry.
@@ -39,7 +39,7 @@ class TestCleanupRegistry:
         """
         Remove test-generated files after all tests complete.
         """
-        for filename in self.FILES_TO_CLEAN:
+        for filename in FILES_TO_CLEAN:
             file_path = os.path.join(self.configdir, filename)
             if os.path.exists(file_path):
                 try:
