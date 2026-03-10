40d04da669b3d4fb03f89fc5c9f28ee21ac42280
Paolo Davini <p.davini@isac.cnr.it>
2026-03-10 10:50:05 +0100

    ecmean

--- src/aqua_diagnostics/ecmean/cli_ecmean.py
+++ src/aqua_diagnostics/ecmean/cli_ecmean.py
@@ -166,7 +166,7 @@ def set_description(diagnostic, model, exp, year1, year2, config):
     Returns:
         description (str)
     """
-    model_time = f"for {model} {exp} from {year1}-01-01 to {year2}-12-31. "
+    model_time = f"for {model} {exp} from {year1}-01 to {year2}-12."
 
     region_bounds = {
         'Global':       (-90.0,  90.0),
@@ -186,12 +186,13 @@ def set_description(diagnostic, model, exp, year1, year2, config):
 
     if diagnostic == 'performance_indices':
         description = (f"Reichler and Kim (2008) Performance Indices (normalized against an ensemble of CMIP6 models) "
-                       f"for different regions and seasons {model_time}"
-                       f"{regions_phrase}. Numbers < 1 imply better results than CMIP6 mean.")
+                       f"for different regions and seasons {model_time} "
+                       f"{regions_phrase}. Values smaller than one imply better results than the CMIP6 multi-model mean.")
     elif diagnostic == 'global_mean':
-        description = (f"Global mean biases normalized to observed interannual variability "
-                       f"with respect to references for different regions and seasons {model_time}"
-                       f"{regions_phrase}")
+        description = (f"Global mean differences with respect to observational references "
+                       f"(normalized to obsevational interannual variability) "
+                       f" for different regions and seasons {model_time} "
+                       f"{regions_phrase}. Darker colors implies larger differences.")
     else:
         # produce a generic description
         description = f"Diagnostic {diagnostic} {model_time.strip()}"
