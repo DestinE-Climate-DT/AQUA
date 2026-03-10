6521f3f2ca9279561c374ff11cc47f394edd743c
Jost von Hardenberg <hardenberg@gmail.com>
2026-03-10 23:01:52 +0100

    space2

--- src/aqua_diagnostics/timeseries/plot_timeseries.py
+++ src/aqua_diagnostics/timeseries/plot_timeseries.py
@@ -166,7 +166,7 @@ class PlotTimeseries(PlotBaseMixin):
         """
         description = super().set_description(diagnostic='Time series')
         #TODO: info on yearly and montlhly data should be controlled if the data are actually plotted
-        description += 'Dashed lines represent yearly data, solid lines represent monthly data.'
+        description += ' Dashed lines represent yearly data, solid lines represent monthly data.'
         return description
 
     def plot_timeseries(self, data_labels=None, ref_label=None, title=None):
