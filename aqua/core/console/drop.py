860e4d02546df09e4384e9976f75bc892faabcee
Emanuele Tovazzi <tovaz92@gmail.com>
2026-03-26 12:08:19 +0100

    add stat_kwargs

--- aqua/core/console/drop.py
+++ aqua/core/console/drop.py
@@ -144,7 +144,7 @@ def drop_execute(args):
     drop_cli(args=args, config=config, catalog=catalog, resolution=resolution,
              frequency=frequency, fix=fix, enddate=enddate, startdate=startdate,
              outdir=outdir, tmpdir=tmpdir, loglevel=loglevel,
-             region=region, stat=stat, compact=compact,
+             region=region, stat=stat, stat_kwargs=stat_kwargs, compact=compact,
              definitive=definitive, overwrite=overwrite, rebuild=rebuild,
              default_workers=default_workers, engine=engine, chunks=chunks,
              monitoring=monitoring, do_zarr=do_zarr, verify_zarr=verify_zarr, only_catalog=only_catalog)
