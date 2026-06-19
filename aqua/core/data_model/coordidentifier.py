3383443b9e469bb4f67cb6e57df19b51396f5d73
Paolo Davini <paolo.davini@cnr.it>
2026-06-19 17:54:14 +0200

    extensions of data model

--- aqua/core/data_model/coordidentifier.py
+++ aqua/core/data_model/coordidentifier.py
@@ -467,7 +467,8 @@ class CoordIdentifier:
         if coord.attrs.get("axis") == "Z":
             score += self.SCORE_WEIGHTS["axis"]  # Use axis weight for substring match
             matched.append("axis")
-        if is_meter(coord.attrs.get("units")):
+        # HACK to detect depth in NEMO model output, which uses "NEMO model levels" in units attribute
+        if is_meter(coord.attrs.get("units") or "NEMO" in coord.attrs.get("units", "")):
             score += self.SCORE_WEIGHTS["units"]
             matched.append("units")
         if "depth" in coord.attrs.get("long_name", ""):
