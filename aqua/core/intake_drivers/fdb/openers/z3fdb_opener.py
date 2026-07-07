b4614f46e7484a146bfecb4d937b57cdea8d6e4b
Jost von Hardenberg <jost.hardenberg@polito.it>
2026-07-07 22:18:12 +0000

    pickle issues

--- aqua/core/intake_drivers/fdb/openers/z3fdb_opener.py
+++ aqua/core/intake_drivers/fdb/openers/z3fdb_opener.py
@@ -1,6 +1,8 @@
 # Derived from the fdb-xarray library
 # https://github.com/koldunovn/fdb-xarray
 
+import copyreg
+
 import astropy_healpix
 import dask.array as da
 import numpy as np
@@ -11,11 +13,59 @@ import zarr
 # Test if z3fdb module is available
 try:
     from z3fdb import AxisDefinition, Chunking, ExtractorType, SimpleStoreBuilder
+    from z3fdb._internal.zarr import FdbZarrStore
 
     z3fdb_available = True
 except ImportError:
     z3fdb_available = False
     z3fdb_error_cause = "z3fdb cannot be imported."
+
+# FdbZarrStore (which accesses FDB data) embeds a C++ wrapped ChunkedDataView that cannot be pickled.
+# When running on a Dask distributed cluster, this prevented Dask from serializing
+# and sending the tasks to the workers.
+# We resolve this by registering a custom pickle reduction handler for FdbZarrStore
+# using copyreg.pickle, and capturing the initialization parameters required to reconstruct
+# the store on the Dask workers.
+
+
+def rebuild_fdb_zarr_store(config, mars, serialized_axes, extractor_type_str):
+    """Rebuild an FdbZarrStore from serialized parameters. Used for pickling/Dask serialization."""
+    from z3fdb import AxisDefinition, Chunking, ExtractorType, SimpleStoreBuilder
+
+    builder = SimpleStoreBuilder(config)
+    axes = []
+    for keys, chunking_str in serialized_axes:
+        chunking = getattr(Chunking, chunking_str)
+        axes.append(AxisDefinition(keys, chunking))
+    extractor_type = getattr(ExtractorType, extractor_type_str)
+    builder.add_part(mars, axes, extractor_type)
+    store = builder.build()
+
+    # Attach serialization attributes to the rebuilt store as well
+    store._config = config
+    store._mars = mars
+    store._serialized_axes = serialized_axes
+    store._extractor_type_str = extractor_type_str
+
+    return store
+
+
+def reduce_fdb_zarr_store(store):
+    """Reduce FdbZarrStore to a serializable state for pickling/Dask serialization."""
+    has_attrs = (
+        hasattr(store, "_config")
+        and hasattr(store, "_mars")
+        and hasattr(store, "_serialized_axes")
+        and hasattr(store, "_extractor_type_str")
+    )
+    if not has_attrs:
+        raise TypeError("Cannot pickle FdbZarrStore: missing serialization attributes")
+    return (rebuild_fdb_zarr_store, (store._config, store._mars, store._serialized_axes, store._extractor_type_str))
+
+
+# Register custom pickle reducer if z3fdb is available
+if z3fdb_available:
+    copyreg.pickle(FdbZarrStore, reduce_fdb_zarr_store)
 
 
 def _check_availability():
@@ -305,6 +355,13 @@ def open_z3fdb(
     builder = SimpleStoreBuilder(config)
     builder.add_part(mars, axes, ExtractorType.GRIB)
     store = builder.build()
+
+    # Attach serialization attributes for pickling/Dask support
+    store._config = config
+    store._mars = mars
+    store._serialized_axes = [(axis.keys, axis.chunking.name) for axis in axes]
+    store._extractor_type_str = ExtractorType.GRIB.name
+
     zarr_arr = zarr.open_array(store, mode="r", zarr_format=3, use_consolidated=False)
 
     ds = to_dataset(
