c40bff89c5f62b42097da6e0a15247492e6e932f
Paolo Davini <paolo.davini@cnr.it>
2026-07-07 15:32:44 +0200

    debug level

--- aqua/core/backend/backend_intake_xarray.py
+++ aqua/core/backend/backend_intake_xarray.py
@@ -95,7 +95,7 @@ class BackendIntakeXarray(Backend, CatalogMixin):
         # if the catalog uses CDS api, get the key from user configuration
         if "cds" in self.esmcat.metadata.get("key"):
             cdsapi_key = get_cdsapi_key()
-            self.logger.warning("CDS API %s", cdsapi_key)
+            self.logger.debug("CDS API %s", cdsapi_key)
             read_kwargs["storage_options"] = {"headers": {"Authorization": f"Bearer {cdsapi_key}"}}
 
         return read_kwargs
