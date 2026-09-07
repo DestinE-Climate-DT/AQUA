faad0ff2f1ef293d8b46c61d354690fe6dfc04c5
Jost von Hardenberg <hardenberg@gmail.com>
2026-09-07 21:50:22 +0200

    pd.period

--- aqua/core/backend/backend.py
+++ aqua/core/backend/backend.py
@@ -162,7 +162,9 @@ class Backend(ABC):
     def _seldate(self, data: xr.Dataset, startdate: str = None, enddate: str = None):
         """Store date bounds for lazy application."""
         t_start = pd.Timestamp(startdate) if startdate else None
-        t_end = pd.Timestamp(enddate) if enddate else None
+        # This guarantees that the end of year, day, hour etc is correctly selected
+        t_end = pd.Period(enddate).end_time if enddate else None
+        # t_end = pd.Timestamp(enddate) if enddate else None
         return data.sel(time=slice(t_start, t_end))
 
     def _sellevel(
