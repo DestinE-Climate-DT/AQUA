955a6db00046d6d9a49334307440b0b57ac7e58b
oloapinivad <paolo.davini@cnr.it>
2026-09-11 17:01:33 +0200

    fix tests

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -408,7 +408,7 @@ class TestIntegrationFlow:
         analysis.catalog = "test_catalog"
         analysis.realization = "r1"
         analysis.output_dir = temp_env["outdir"]
-        analysis.cluster = MagicMock()
+        analysis.dask_cluster = MagicMock()
 
         config = {"biases": {"config": temp_env["cfg_biases"]}}
 
