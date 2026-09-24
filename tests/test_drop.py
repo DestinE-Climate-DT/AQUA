eab9f428b88bdd3cbedc69cbd1b4e46b27f68963
mnurisso <m.nurisso@isac.cnr.it>
2026-09-24 11:09:52 +0200

    new test aligned to xarray only option for zarr, cdo does turn off compact method

--- tests/test_drop.py
+++ tests/test_drop.py
@@ -380,6 +380,7 @@ class TestDROP:
             ("zarr", "xarray", 3, False),  # <12 months: monthly stores remain
             ("zarr", "xarray", 12, True),  # 12 months: yearly store created
             ("zarr", None, 12, False),  # null compacting: monthly stores remain
+            ("zarr", "cdo", 12, False),  # cdo is invalid for zarr: compaction is disabled
         ],
     )
     def test_concat_threshold(self, drop_arguments, tmp_path, output_format, compact_method, num_months, should_concat):
